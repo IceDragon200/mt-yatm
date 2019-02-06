a5b9d93ecdb0a1e6aff51ad86fffcf56834d8ab1
Corey Powell <mistdragon100@gmail.com>
2019-02-05 23:32:23 -0500

    Moved energy tests to bottom of file

--- yatm_core/energy.lua
+++ yatm_core/energy.lua
@@ -3,6 +3,12 @@ Provides some utility functions for dealing with energy in YATM
 ]]
 local energy = {}
 
+energy.schema = yatm_core.MetaSchema.new("energy", "", {
+  energy = {
+    type = "integer",
+  },
+})
+
 -- Helper function for dealing with energy
 function energy.allowed_energy(energy, bandwidth)
   assert(energy, "expected energy to be present")
@@ -24,30 +30,6 @@ function energy.calc_consumed_energy(stored_energy, amount, bandwidth, _capacity
   local new_stored_energy = math.max(stored_energy - new_amount, 0)
   return new_stored_energy, stored_energy - new_stored_energy
 end
-
-do
-  assert(energy.allowed_energy(100, 10) == 10, "expected allowed_energy to limit given energy by the bandwidth")
-  assert(energy.allowed_energy(100, nil) == 100, "expected allowed_energy given a nil bandwidth to return energy")
-  assert(energy.allowed_energy(10, 100) == 10, "expected allowed_energy given a bandwidth greater than the energy to return it as is")
-end
-
-do
-  local new_energy, actual_amount = energy.calc_received_energy(15, 10, 10, 20)
-  assert(new_energy == 20, "expected new energy to be 20 but got " .. new_energy)
-  assert(actual_amount == 5, "expected actual_amount to be lower than the given")
-end
-
-do
-  local new_energy, actual_amount = energy.calc_consumed_energy(5, 15, 10, 20)
-  assert(new_energy == 0, "expected new energy to be 0 but got " .. new_energy)
-  assert(actual_amount == 5, "expected actual_amount to be lower than the given")
-end
-
-energy.schema = yatm_core.MetaSchema.new("energy", "", {
-  energy = {
-    type = "integer",
-  },
-})
 
 function energy.get_energy(meta, key)
   return energy.schema:get_field(meta, key, "energy")
@@ -96,3 +78,22 @@ function energy.consume_energy(meta, key, amount, bandwidth, capacity, commit)
 end
 
 yatm_core.energy = energy
+
+-- Tests
+do
+  assert(energy.allowed_energy(100, 10) == 10, "expected allowed_energy to limit given energy by the bandwidth")
+  assert(energy.allowed_energy(100, nil) == 100, "expected allowed_energy given a nil bandwidth to return energy")
+  assert(energy.allowed_energy(10, 100) == 10, "expected allowed_energy given a bandwidth greater than the energy to return it as is")
+end
+
+do
+  local new_energy, actual_amount = energy.calc_received_energy(15, 10, 10, 20)
+  assert(new_energy == 20, "expected new energy to be 20 but got " .. new_energy)
+  assert(actual_amount == 5, "expected actual_amount to be lower than the given")
+end
+
+do
+  local new_energy, actual_amount = energy.calc_consumed_energy(5, 15, 10, 20)
+  assert(new_energy == 0, "expected new energy to be 0 but got " .. new_energy)
+  assert(actual_amount == 5, "expected actual_amount to be lower than the given")
+end
