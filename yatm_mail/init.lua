8b88cedcf0170855a7eb3137d37a6564adbefa54
Corey Powell <mistdragon100@gmail.com>
2019-02-26 22:39:15 -0500

    mail: Added mail module

---
+++ yatm_mail/init.lua
@@ -0,0 +1,5 @@
+--[[
+YATM Mail, offers simple mailboxes for dropping off letters
+]]
+yatm_mail = rawget(_G, "yatm_mail") or {}
+yatm_mail.modpath = minetest.get_modpath(minetest.get_current_modname())
