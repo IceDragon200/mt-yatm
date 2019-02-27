8369814fdb330990c2c6f5574065703c42847e25
Corey Powell <mistdragon100@gmail.com>
2019-02-26 22:39:08 -0500

    armoury: Added init script

---
+++ yatm_armoury/init.lua
@@ -0,0 +1,7 @@
+--[[
+YATM Armoury
+
+Offers weapons, armor and other items geared towards fights
+]]
+yatm_armoury = rawget(_G, "yatm_armoury") or {}
+yatm_armoury.modpath = minetest.get_modpath(minetest.get_current_modname())
