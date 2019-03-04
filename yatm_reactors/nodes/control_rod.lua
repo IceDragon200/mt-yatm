3965696aeae766cefaf01bfa1c6b08e0310ad98d
Corey Powell <mistdragon100@gmail.com>
2019-03-03 23:13:17 -0500

    yatm_reactors: Fixed missing yatm_network for control rod

--- yatm_reactors/nodes/control_rod.lua
+++ yatm_reactors/nodes/control_rod.lua
@@ -1,3 +1,18 @@
+local control_rod_open_yatm_network = {
+  kind = "machine",
+  groups = {
+    reactor = 1,
+    reactor_control_rod = 1,
+    reactor_control_rod_open = 1,
+  },
+  states = {
+    conflict = "yatm_reactors:control_rod_open",
+    error = "yatm_reactors:control_rod_open",
+    off = "yatm_reactors:control_rod_open",
+    on = "yatm_reactors:control_rod_open",
+  }
+}
+
 yatm_machines.register_network_device("yatm_reactors:control_rod_open", {
   description = "Control Rod (Unoccupied)",
   groups = {cracky = 1},
@@ -12,7 +27,7 @@ yatm_machines.register_network_device("yatm_reactors:control_rod_open", {
   },
   paramtype = "light",
   paramtype2 = "facedir",
-  yatm_network = control_rod_yatm_network,
+  yatm_network = control_rod_open_yatm_network,
 })
 
 for _, variant in ipairs({"uranium", "plutonium", "radium"}) do
