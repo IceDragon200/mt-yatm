3cda8b5c35b23014ab67418e1206b7459fedcb23
Corey Powell <mistdragon100@gmail.com>
2019-03-06 23:56:43 -0500

    yatm_mail: Locksmith's table now has wood sounds

--- yatm_mail/nodes/locksmiths_table.lua
+++ yatm_mail/nodes/locksmiths_table.lua
@@ -83,6 +83,8 @@ minetest.register_node("yatm_mail:locksmiths_table_wood", {
   drawtype = "nodebox",
   node_box = table_nodebox,
 
+  sounds = default.node_sound_wood_defaults(),
+
   on_construct = locksmiths_table_on_construct,
   on_destruct = locksmiths_table_on_destruct,
 })
