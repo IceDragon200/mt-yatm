5ba5e85f71cfa07273c20919dbc496b506dd1b89
Corey Powell <mistdragon100@gmail.com>
2019-03-09 02:42:06 -0500

    yatm_core: Added some utility functions for itemstacks

--- yatm_core/util.lua
+++ yatm_core/util.lua
@@ -155,3 +155,65 @@ function yatm_core.format_pretty_unit(value, unit)
   end
   return result .. unit
 end
+
+function yatm_core.inspect_itemstack(stack)
+  if stack then
+    return "stack[" .. stack:get_name() .. "/" .. stack:get_count() .. "]"
+  else
+    return "nil"
+  end
+end
+
+function yatm_core.is_blank(value)
+  if value == nil then
+    return true
+  elseif value == "" then
+    return true
+  else
+    return false
+  end
+end
+
+function yatm_core.itemstack_is_blank(stack)
+  if stack then
+    return yatm_core.is_blank(stack:get_name()) or stack:get_count() == 0
+  else
+    return true
+  end
+end
+
+local STRING_POOL = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+function yatm_core.random_string(length, pool)
+  pool = pool or STRING_POOL
+  local pool_size = #pool
+  local result = {}
+  for i = 1,length do
+    local pos = math.random(pool_size - 1)
+    result[i] = assert(string.sub(pool, pos, pos))
+  end
+  return table.concat(result)
+end
+
+local function assert_itemstack_meta(itemstack)
+  if not itemstack or not itemstack.get_meta then
+    error("expected an itemstack with get_meta function (got " .. dump(itemstack) .. ")")
+  end
+end
+
+function yatm_core.get_itemstack_description(itemstack)
+  assert_itemstack_meta(itemstack)
+  local desc = itemstack:get_meta():get_string("description")
+  if yatm_core.is_blank(desc) then
+    local itemdef = minetest.registered_items[itemstack:get_name()]
+    return itemdef.description or itemstack:get_name()
+  else
+    return desc
+  end
+end
+
+function yatm_core.set_itemstack_meta_description(itemstack, description)
+  assert_itemstack_meta(itemstack)
+  itemstack:get_meta():set_string("description", description)
+  return itemstack
+end
