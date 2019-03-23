b561a7f5566db555b6e4ddc93a0b9d6dbe72a714
Corey Powell <mistdragon100@gmail.com>
2019-03-23 10:16:13 -0500

    yatm_decor: Added mesh and vent decor nodes

---
+++ yatm_decor/nodes/meshes.lua
@@ -0,0 +1,25 @@
+minetest.register_node("yatm_decor:mesh_dense", {
+  description = "Dense Mesh",
+  groups = {cracky = 1},
+  tiles = {
+    "yatm_meshes_border.png",
+    "yatm_meshes_dense_mesh.png",
+  },
+  drawtype = "glasslike_framed",
+  place_param2 = 0,
+  paramtype = "light",
+  paramtype2 = "facedir",
+})
+
+minetest.register_node("yatm_decor:mesh_wide", {
+  description = "Wide Mesh",
+  groups = {cracky = 1},
+  tiles = {
+    "yatm_meshes_border.png",
+    "yatm_meshes_wide_mesh.png",
+  },
+  drawtype = "glasslike_framed",
+  place_param2 = 0,
+  paramtype = "light",
+  paramtype2 = "facedir",
+})
---
+++ yatm_decor/nodes/vents.lua
@@ -0,0 +1,10 @@
+minetest.register_node("yatm_decor:vent", {
+  description = "Vent",
+  groups = {cracky = 1},
+  tiles = {
+    "yatm_vents_vent.png",
+  },
+  place_param2 = 0,
+  paramtype = "light",
+  paramtype2 = "facedir",
+})
