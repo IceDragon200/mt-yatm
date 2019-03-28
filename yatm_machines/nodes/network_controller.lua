22c823b64a468202cc178c0766b23c87b31edc65
Corey Powell <mistdragon100@gmail.com>
2019-03-27 23:25:05 -0500

    yatm_machines: Network controller changes
    It's now a valid stateful and has metal sounds

--- yatm_machines/nodes/network_controller.lua
+++ yatm_machines/nodes/network_controller.lua
@@ -1,23 +1,29 @@
-local network_yatm_network = {
+local network_controller_yatm_network = {
   kind = "controller",
   groups = {
     controller = 1,
   },
+  default_state = "off",
   states = {
     conflict = "yatm_machines:network_controller_error",
     error = "yatm_machines:network_controller_error",
     on = "yatm_machines:network_controller_on",
     off = "yatm_machines:network_controller_off",
-  }
+  },
 }
 
 local groups = {
   cracky = 1, yatm_network_host = 1, yatm_data_device = 1, yatm_energy_device = 1,
 }
 
-yatm.devices.register_network_device("yatm_machines:network_controller_off", {
+yatm.devices.register_stateful_network_device({
   description = "Network Controller",
   groups = groups,
+
+  drop = network_controller_yatm_network.states.off,
+
+  sounds = default.node_sound_metal_defaults(),
+
   tiles = {
     "yatm_network_controller_top.off.png",
     "yatm_network_controller_bottom.png",
@@ -26,65 +32,55 @@ yatm.devices.register_network_device("yatm_machines:network_controller_off", {
     "yatm_network_controller_back.off.png",
     "yatm_network_controller_front.off.png",
   },
-  paramtype = "light",
-  paramtype2 = "facedir",
-  yatm_network = network_yatm_network,
-})
 
-yatm.devices.register_network_device("yatm_machines:network_controller_error", {
-  description = "Network Controller",
-  drop = "yatm_machines:network_controller_off",
-  groups = yatm_core.table_merge(groups, {not_in_creative_inventory = 1}),
-  tiles = {
-    "yatm_network_controller_top.error.png",
-    "yatm_network_controller_bottom.png",
-    "yatm_network_controller_side.error.png",
-    "yatm_network_controller_side.error.png^[transformFX",
-    "yatm_network_controller_back.error.png",
-    "yatm_network_controller_front.error.png",
-  },
   paramtype = "light",
   paramtype2 = "facedir",
-  yatm_network = network_yatm_network,
-})
 
-yatm.devices.register_network_device("yatm_machines:network_controller_on", {
-  description = "Network Controller",
-  drop = "yatm_machines:network_controller_off",
-  groups = yatm_core.table_merge(groups, {not_in_creative_inventory = 1}),
-  tiles = {
-    {
-      name = "yatm_network_controller_top.on.png",
-      animation = {
-        type = "vertical_frames",
-        aspect_w = 16,
-        aspect_h = 16,
-        length = 2.0
-      },
+  yatm_network = network_controller_yatm_network,
+
+}, {
+  error = {
+    tiles = {
+      "yatm_network_controller_top.error.png",
+      "yatm_network_controller_bottom.png",
+      "yatm_network_controller_side.error.png",
+      "yatm_network_controller_side.error.png^[transformFX",
+      "yatm_network_controller_back.error.png",
+      "yatm_network_controller_front.error.png",
     },
-    "yatm_network_controller_bottom.png",
-    {
-      name = "yatm_network_controller_side.on.png",
-      animation = {
-        type = "vertical_frames",
-        aspect_w = 16,
-        aspect_h = 16,
-        length = 2.0
-      },
-    },
-    {
-      name = "yatm_network_controller_side.on.png^[transformFX",
-      animation = {
-        type = "vertical_frames",
-        aspect_w = 16,
-        aspect_h = 16,
-        length = 2.0
-      },
-    },
-    "yatm_network_controller_back.on.png",
-    "yatm_network_controller_front.on.png",
   },
-  paramtype = "light",
-  paramtype2 = "facedir",
-  yatm_network = network_yatm_network,
+  on = {
+    tiles = {
+      {
+        name = "yatm_network_controller_top.on.png",
+        animation = {
+          type = "vertical_frames",
+          aspect_w = 16,
+          aspect_h = 16,
+          length = 2.0
+        },
+      },
+      "yatm_network_controller_bottom.png",
+      {
+        name = "yatm_network_controller_side.on.png",
+        animation = {
+          type = "vertical_frames",
+          aspect_w = 16,
+          aspect_h = 16,
+          length = 2.0
+        },
+      },
+      {
+        name = "yatm_network_controller_side.on.png^[transformFX",
+        animation = {
+          type = "vertical_frames",
+          aspect_w = 16,
+          aspect_h = 16,
+          length = 2.0
+        },
+      },
+      "yatm_network_controller_back.on.png",
+      "yatm_network_controller_front.on.png",
+    }
+  },
 })
