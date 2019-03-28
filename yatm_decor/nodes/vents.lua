1eacb19396db2fa3c08115b67783c2d9a3ac2914
Corey Powell <mistdragon100@gmail.com>
2019-03-27 23:25:53 -0500

    yatm_decor: Vents have metal sounds

--- yatm_decor/nodes/vents.lua
+++ yatm_decor/nodes/vents.lua
@@ -1,9 +1,12 @@
 minetest.register_node("yatm_decor:vent", {
   description = "Vent",
   groups = {cracky = 1},
+
   tiles = {
     "yatm_vents_vent.png",
   },
   paramtype = "light",
   paramtype2 = "facedir",
+
+  sounds = default.node_sound_metal_defaults(),
 })
