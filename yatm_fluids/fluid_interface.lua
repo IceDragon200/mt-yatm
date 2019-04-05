973a7caad90761f1567931c9cb2b219d89e4f100
Corey Powell <mistdragon100@gmail.com>
2019-04-05 16:51:55 -0500

    yatm_fluids: FluidInterface default functions are now defined outside

--- yatm_fluids/fluid_interface.lua
+++ yatm_fluids/fluid_interface.lua
@@ -15,24 +15,77 @@ local function default_on_fluid_changed(self, pos, dir, new_stack)
   -- Do nothing
 end
 
+local function default_allow_replace(self, pos, dir, new_stack)
+  return true
+end
+
+local function default_allow_fill(self, pos, dir, fluid_stack)
+  return true
+end
+
+local function default_allow_drain(self, pos, dir, fluid_stack)
+  return true
+end
+
 function FluidInterface.new()
   local fluid_interface = {
     on_fluid_changed = default_on_fluid_changed,
+    allow_replace = default_allow_replace,
+    allow_fill = default_allow_fill,
+    allow_drain = default_allow_drain,
   }
 
-  function fluid_interface:allow_replace(pos, dir, new_stack)
-    return true
-  end
-
-  function fluid_interface:allow_fill(pos, dir, fluid_stack)
-    return true
-  end
-
-  function fluid_interface:allow_drain(pos, dir, fluid_stack)
-    return true
-  end
-
   return fluid_interface
+end
+
+local function default_simple_get(self, pos, dir)
+  local meta = minetest.get_meta(pos)
+  local stack = FluidMeta.get_fluid(meta, self.tank_name)
+  return stack
+end
+
+function default_simple_replace(self, pos, dir, new_stack, commit)
+  if self:allow_replace(pos, dir, new_stack) then
+    local meta = minetest.get_meta(pos)
+    local stack, new_stack = FluidMeta.set_fluid(meta,
+      self.tank_name,
+      new_stack,
+      commit)
+    if commit then
+      self:on_fluid_changed(pos, dir, new_stack)
+    end
+    return stack
+  else
+    return nil
+  end
+end
+
+function default_simple_fill(self, pos, dir, fluid_stack, commit)
+  if self:allow_fill(pos, dir, fluid_stack) then
+    local meta = minetest.get_meta(pos)
+    local stack, new_stack = FluidMeta.fill_fluid(meta,
+      self.tank_name,
+      fluid_stack,
+      self.bandwidth, self.capacity, commit)
+    if commit then
+      self:on_fluid_changed(pos, dir, new_stack)
+    end
+    return stack
+  end
+end
+
+function default_simple_drain(self, pos, dir, fluid_stack, commit)
+  if self:allow_drain(pos, dir, fluid_stack) then
+    local meta = minetest.get_meta(pos)
+    local stack, new_stack = FluidMeta.drain_fluid(meta,
+      self.tank_name,
+      fluid_stack,
+      self.bandwidth, self.capacity, commit)
+    if commit then
+      self:on_fluid_changed(pos, dir, new_stack)
+    end
+    return stack
+  end
 end
 
 function FluidInterface.new_simple(tank_name, capacity)
@@ -42,57 +95,76 @@ function FluidInterface.new_simple(tank_name, capacity)
   fluid_interface.bandwidth = capacity
   fluid_interface.tank_name = tank_name
 
-  function fluid_interface:get(pos, dir)
-    local meta = minetest.get_meta(pos)
-    local stack = FluidMeta.get_fluid(meta, self.tank_name)
+  fluid_interface.get = default_simple_get
+  fluid_interface.replace = default_simple_replace
+  fluid_interface.fill = default_simple_fill
+  fluid_interface.drain = default_simple_drain
+
+  return fluid_interface
+end
+
+local function default_directional_get(self, pos, dir)
+  local meta = minetest.get_meta(pos)
+  local tank_name, _capacity = self:get_fluid_tank_name(pos, dir)
+  if tank_name then
+    local stack = FluidMeta.get_fluid(meta, tank_name)
     return stack
   end
+  return nil
+end
 
-  function fluid_interface:replace(pos, dir, new_stack, commit)
-    if self:allow_replace(pos, dir, new_stack) then
-      local meta = minetest.get_meta(pos)
+local function default_directional_replace(self, pos, dir, fluid_stack, commit)
+  if self:allow_replace(pos, dir, fluid_stack) then
+    local meta = minetest.get_meta(pos)
+    local tank_name, _capacity = self:get_fluid_tank_name(pos, dir)
+    if tank_name then
       local stack, new_stack = FluidMeta.set_fluid(meta,
-        self.tank_name,
-        new_stack,
+        tank_name,
+        fluid_stack,
         commit)
       if commit then
         self:on_fluid_changed(pos, dir, new_stack)
       end
       return stack
-    else
-      return nil
     end
   end
+  return nil
+end
 
-  function fluid_interface:fill(pos, dir, fluid_stack, commit)
-    if self:allow_fill(pos, dir, fluid_stack) then
-      local meta = minetest.get_meta(pos)
+local function default_directional_fill(self, pos, dir, fluid_stack, commit)
+  if self:allow_fill(pos, dir, fluid_stack) then
+    local meta = minetest.get_meta(pos)
+    local tank_name, capacity = self:get_fluid_tank_name(pos, dir)
+    if tank_name then
       local stack, new_stack = FluidMeta.fill_fluid(meta,
-        self.tank_name,
+        tank_name,
         fluid_stack,
-        self.bandwidth, self.capacity, commit)
+        capacity, capacity, commit)
       if commit then
         self:on_fluid_changed(pos, dir, new_stack)
       end
       return stack
     end
   end
+  return nil
+end
 
-  function fluid_interface:drain(pos, dir, fluid_stack, commit)
-    if self:allow_drain(pos, dir, fluid_stack) then
-      local meta = minetest.get_meta(pos)
+local function default_directional_drain(self, pos, dir, fluid_stack, commit)
+  if self:allow_drain(pos, dir, fluid_stack) then
+    local meta = minetest.get_meta(pos)
+    local tank_name, capacity = self:get_fluid_tank_name(pos, dir)
+    if tank_name then
       local stack, new_stack = FluidMeta.drain_fluid(meta,
-        self.tank_name,
+        tank_name,
         fluid_stack,
-        self.bandwidth, self.capacity, commit)
+        capacity, capacity, commit)
       if commit then
         self:on_fluid_changed(pos, dir, new_stack)
       end
       return stack
     end
   end
-
-  return fluid_interface
+  return nil
 end
 
 function FluidInterface.new_directional(get_fluid_tank_name)
@@ -100,69 +172,10 @@ function FluidInterface.new_directional(get_fluid_tank_name)
   fluid_interface.get_fluid_tank_name = get_fluid_tank_name
   fluid_interface.on_fluid_changed = default_on_fluid_changed
 
-  function fluid_interface:get(pos, dir)
-    local meta = minetest.get_meta(pos)
-    local tank_name, _capacity = self:get_fluid_tank_name(pos, dir)
-    if tank_name then
-      local stack = FluidMeta.get_fluid(meta, tank_name)
-      return stack
-    end
-    return nil
-  end
-
-  function fluid_interface:replace(pos, dir, fluid_stack, commit)
-    if self:allow_replace(pos, dir, fluid_stack) then
-      local meta = minetest.get_meta(pos)
-      local tank_name, _capacity = self:get_fluid_tank_name(pos, dir)
-      if tank_name then
-        local stack, new_stack = FluidMeta.set_fluid(meta,
-          tank_name,
-          fluid_stack,
-          commit)
-        if commit then
-          self:on_fluid_changed(pos, dir, new_stack)
-        end
-        return stack
-      end
-    end
-    return nil
-  end
-
-  function fluid_interface:fill(pos, dir, fluid_stack, commit)
-    if self:allow_fill(pos, dir, fluid_stack) then
-      local meta = minetest.get_meta(pos)
-      local tank_name, capacity = self:get_fluid_tank_name(pos, dir)
-      if tank_name then
-        local stack, new_stack = FluidMeta.fill_fluid(meta,
-          tank_name,
-          fluid_stack,
-          capacity, capacity, commit)
-        if commit then
-          self:on_fluid_changed(pos, dir, new_stack)
-        end
-        return stack
-      end
-    end
-    return nil
-  end
-
-  function fluid_interface:drain(pos, dir, fluid_stack, commit)
-    if self:allow_drain(pos, dir, fluid_stack) then
-      local meta = minetest.get_meta(pos)
-      local tank_name, capacity = self:get_fluid_tank_name(pos, dir)
-      if tank_name then
-        local stack, new_stack = FluidMeta.drain_fluid(meta,
-          tank_name,
-          fluid_stack,
-          capacity, capacity, commit)
-        if commit then
-          self:on_fluid_changed(pos, dir, new_stack)
-        end
-        return stack
-      end
-    end
-    return nil
-  end
+  fluid_interface.get = default_directional_get
+  fluid_interface.replace = default_directional_replace
+  fluid_interface.fill = default_directional_fill
+  fluid_interface.drain = default_directional_drain
 
   return fluid_interface
 end
