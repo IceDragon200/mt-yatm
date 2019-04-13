166af860b8b38ac25b41c3f8636e1d33e101df4e
Corey Powell <mistdragon100@gmail.com>
2019-04-12 20:52:08 -0500

    yatm_foundry: Copy paste molder

--- yatm_foundry/nodes/molder.lua
+++ yatm_foundry/nodes/molder.lua
@@ -1,39 +1,263 @@
+--[[
+A non-electric version of the molder, unfortunately for you it requires heat instead of energy!
+
+Reason? Balance, otherwise you could just abuse the poor thing for all your molding needs.
+
+That or just make it god awful slow at it's job.
+]]
+local HeatInterface = assert(yatm.heating.HeatInterface)
+local FluidInterface = assert(yatm.fluids.FluidInterface)
+local FluidStack = assert(yatm.fluids.FluidStack)
+local FluidMeta = assert(yatm.fluids.FluidMeta)
+local ItemInterface = assert(yatm.items.ItemInterface)
+local MoldingRegistry = assert(yatm.molding.MoldingRegistry)
+
+local function get_molder_formspec(pos)
+  local spos = pos.x .. "," .. pos.y .. "," .. pos.z
+  local formspec =
+    "size[8,9]" ..
+    "list[nodemeta:" .. spos .. ";mold_slot;0,0.3;1,1;]" ..
+    "list[nodemeta:" .. spos .. ";molding_slot;2,0.3;1,1;]" ..
+    "list[nodemeta:" .. spos .. ";output_slot;4,0.3;1,1;]" ..
+    "list[current_player;main;0,4.85;8,1;]" ..
+    "list[current_player;main;0,6.08;8,3;8]" ..
+    "listring[nodemeta:" .. spos .. ";mold_slot]" ..
+    "listring[current_player;main]" ..
+    "listring[nodemeta:" .. spos .. ";output_slot]" ..
+    "listring[current_player;main]" ..
+    default.get_hotbar_bg(0,4.85)
+  return formspec
+end
+
+local heat_interface = HeatInterface.new_simple("heat", 400)
+function heat_interface:on_heat_changed(pos, node, old_heat, new_heat)
+  if math.floor(new_heat) > 0 then
+    minetest.swap_node(pos, {name = "yatm_foundry:molder_on"})
+  else
+    minetest.swap_node(pos, {name = "yatm_foundry:molder_off"})
+  end
+  yatm_core.queue_refresh_infotext(pos)
+  minetest.get_node_timer(pos):start(1.0)
+end
+
+local TANK_CAPACITY = 4000
+local fluid_interface = FluidInterface.new_simple("molten_tank", TANK_CAPACITY)
+
+function fluid_interface:on_fluid_changed(pos, dir, _new_stack)
+  yatm_core.queue_refresh_infotext(pos)
+end
+
+function fluid_interface:allow_replace(pos, dir, fluid_stack)
+  -- If the fluid is molten, then it can be replaced
+  if FluidStack.is_member_of_group(fluid_stack, "molten") then
+    return true
+  else
+    return false, "fluid is not molten"
+  end
+end
+
+fluid_interface.allow_fill = fluid_interface.allow_replace
+fluid_interface.allow_drain = fluid_interface.allow_replace
+
+local item_interface = ItemInterface.new_directional(function (self, pos, dir)
+  local node = minetest.get_node(pos)
+  local new_dir = yatm_core.facedir_to_face(node.param2, dir)
+  if new_dir == yatm_core.D_UP or new_dir == yatm_core.D_DOWN then
+    return "mold_slot"
+  end
+  return "output_slot"
+end)
+
+function molder_refresh_infotext(pos)
+  local meta = minetest.get_meta(pos)
+
+  local heat = math.floor(meta:get_float("heat"))
+
+  local molten_tank_fluid_stack = FluidMeta.get_fluid_stack(meta, "molten_tank")
+  local molding_tank_fluid_stack = FluidMeta.get_fluid_stack(meta, "molding_tank")
+  local recipe_name = meta:get_string("recipe_name")
+  local recipe_time = meta:get_float("recipe_time")
+  local recipe_time_max = meta:get_float("recipe_time_max")
+
+  local infotext =
+    "Heat: " .. heat .. " / " .. heat_interface.heat_capacity .. "\n" ..
+    "Recipe: " .. recipe_name .. "\n" ..
+    "Molten Tank: " .. FluidStack.pretty_format(molten_tank_fluid_stack, fluid_interface.capacity) .. "\n" ..
+    "Molding Tank: " .. FluidStack.pretty_format(molding_tank_fluid_stack, fluid_interface.capacity) .. "\n" ..
+    "Time Remaining: " .. yatm_core.format_pretty_time(recipe_time) .. " / " .. yatm_core.format_pretty_time(recipe_time_max)
+
+  meta:set_string("infotext", infotext)
+end
+
+local function molder_on_construct(pos)
+  yatm.devices.device_on_construct(pos)
+  local meta = minetest.get_meta(pos)
+  local inv = meta:get_inventory()
+  inv:set_size("mold_slot", 1)
+  inv:set_size("molding_slot", 1)
+  inv:set_size("output_slot", 1)
+end
+
+local function molder_on_rightclick(pos, node, clicker)
+  minetest.show_formspec(
+    clicker:get_player_name(),
+    "yatm_foundry:molder",
+    get_molder_formspec(pos)
+  )
+end
+
+local function molder_on_timer(pos, dtime)
+  local meta = minetest.get_meta(pos)
+
+  local available_heat = meta:get_float("heat")
+  if available_heat > 0 then
+    local inv = meta:get_inventory()
+
+    local molding_fluid = FluidMeta.get_fluid_stack(meta, "molding_tank")
+    if not FluidStack.presence(molding_fluid) then
+      local mold_item_stack = inv:get_stack("mold_slot",  1)
+
+      if not yatm_core.itemstack_is_blank(mold_item_stack) then
+        local molten_fluid = FluidMeta.get_fluid_stack(meta, "molten_tank")
+        local recipe = MoldingRegistry:get_molding_recipe(mold_item_stack, molten_fluid)
+        if recipe then
+          local drained_fluid = FluidMeta.drain_fluid(meta, "molten_tank", recipe.molten_fluid, TANK_CAPACITY, TANK_CAPACITY, false)
+          if FluidMeta.room_for_fluid(meta, "molding_tank", drained_fluid, TANK_CAPACITY, TANK_CAPACITY) then
+            meta:set_float("recipe_time", recipe.duration)
+            meta:set_float("recipe_time_max", recipe.duration)
+            meta:set_string("recipe_name", recipe.name)
+            inv:add_item("molding_slot", mold_item_stack)
+            inv:remove_item("mold_slot", mold_item_stack)
+            local filled_fluid = FluidMeta.fill_fluid(meta, "molding_tank", drained_fluid, TANK_CAPACITY, TANK_CAPACITY, true)
+            local drained_fluid = FluidMeta.drain_fluid(meta, "molten_tank", filled_fluid, TANK_CAPACITY, TANK_CAPACITY, true)
+            --print("filled fluid in molten tank", minetest.pos_to_string(pos), FluidStack.pretty_format(filled_fluid))
+            yatm_core.queue_refresh_infotext(pos)
+          end
+        end
+      end
+    end
+
+    local molding_fluid = FluidMeta.get_fluid_stack(meta, "molding_tank")
+    if FluidStack.presence(molding_fluid) then
+      local applyable_dtime = math.min(available_heat / 5.0, dtime)
+      meta:set_float("heat", math.max(available_heat - 5 * applyable_dtime, 0))
+
+      local recipe_time = meta:get_float("recipe_time")
+      recipe_time = math.max(recipe_time - applyable_dtime, 0)
+      meta:set_float("recipe_time", recipe_time)
+      if recipe_time == 0 then
+        local mold_item_stack = inv:get_stack("molding_slot",  1)
+        local recipe = MoldingRegistry:get_molding_recipe(mold_item_stack, molding_fluid)
+
+        if recipe and inv:room_for_item("output_slot", recipe.result_item_stack) then
+          local drained_fluid = FluidMeta.drain_fluid(meta, "molding_tank", recipe.molten_fluid, TANK_CAPACITY, TANK_CAPACITY, false)
+          if drained_fluid and drained_fluid.amount == recipe.molten_fluid.amount then
+            local drained_fluid = FluidMeta.drain_fluid(meta, "molding_tank", recipe.molten_fluid, TANK_CAPACITY, TANK_CAPACITY, true)
+            local result = yatm_core.itemstack_copy(recipe.result_item_stack)
+            --print("drained fluid from molten tank", minetest.pos_to_string(pos), FluidStack.pretty_format(drained_fluid))
+            inv:add_item("output_slot", result)
+            inv:add_item("mold_slot", mold_item_stack)
+            inv:remove_item("molding_slot", mold_item_stack)
+            meta:set_string("recipe_name", "")
+            meta:set_float("recipe_time", 0)
+            meta:set_float("recipe_time_max", 0)
+            yatm_core.queue_refresh_infotext(pos)
+          end
+        end
+      else
+        yatm_core.queue_refresh_infotext(pos)
+      end
+    end
+    return true
+  else
+    return false
+  end
+end
+
 local groups = {
   cracky = 1,
   item_interface_in = 1,
   item_interface_out = 1,
+  heated_device = 1,
+  heat_interface_in = 1,
+}
+
+local node_box = {
+  type = "fixed",
+  fixed = {
+    {-0.5, -0.5, -0.5, 0.5, (12 / 16.0) - 0.5, 0.5}, -- Base
+    {-0.5, (15 / 16.0) - 0.5, -0.5, 0.5, 0.5, 0.5}, -- Cap
+    -- Columns
+    {(1 / 16.0) - 0.5, (12 / 16.0) - 0.5, (1 / 16.0) - 0.5, (3 / 16.0) - 0.5, (15 / 16.0) - 0.5, (3 / 16.0) - 0.5},
+    {(1 / 16.0) - 0.5, (12 / 16.0) - 0.5, (13 / 16.0) - 0.5, (3 / 16.0) - 0.5, (15 / 16.0) - 0.5, (15 / 16.0) - 0.5},
+    {(13 / 16.0) - 0.5, (12 / 16.0) - 0.5, (1 / 16.0) - 0.5, (15 / 16.0) - 0.5, (15 / 16.0) - 0.5, (3 / 16.0) - 0.5},
+    {(13 / 16.0) - 0.5, (12 / 16.0) - 0.5, (13 / 16.0) - 0.5, (15 / 16.0) - 0.5, (15 / 16.0) - 0.5, (15 / 16.0) - 0.5},
+  },
 }
 
 minetest.register_node("yatm_foundry:molder_off", {
   description = "Molder",
   groups = groups,
+
   tiles = {
     "yatm_molder_top.off.png",
     "yatm_molder_bottom.off.png",
     "yatm_molder_side.off.png",
     "yatm_molder_side.off.png^[transformFX",
-    "yatm_molder_back.off.png",
-    "yatm_molder_front.off.png"
+    "yatm_molder_side.off.png",
+    "yatm_molder_side.off.png"
   },
+
+  drawtype = "nodebox",
+  node_box = node_box,
+
   paramtype = "light",
   paramtype2 = "facedir",
 
   sounds = default.node_sound_stone_defaults(),
+
+  fluid_interface = fluid_interface,
+  item_interface = item_interface,
+  heat_interface = heat_interface,
+
+  refresh_infotext = electric_molder_refresh_infotext,
+
+  on_construct = molder_on_construct,
+  on_rightclick = molder_on_rightclick,
+  on_timer = molder_on_timer,
 })
 
 minetest.register_node("yatm_foundry:molder_on", {
   description = "Molder",
+
   groups = yatm_core.table_merge(groups, {not_in_creative_inventory = 1}),
+
+  drop = "yatm_foundry:molder_off",
+
   tiles = {
     "yatm_molder_top.on.png",
     "yatm_molder_bottom.on.png",
     "yatm_molder_side.on.png",
     "yatm_molder_side.on.png^[transformFX",
-    "yatm_molder_back.on.png",
-    "yatm_molder_front.on.png"
+    "yatm_molder_side.on.png",
+    "yatm_molder_side.on.png"
   },
+
+  drawtype = "nodebox",
+  node_box = node_box,
+
   paramtype = "light",
   paramtype2 = "facedir",
 
   sounds = default.node_sound_stone_defaults(),
+
+  fluid_interface = fluid_interface,
+  item_interface = item_interface,
+  heat_interface = heat_interface,
+
+  refresh_infotext = electric_molder_refresh_infotext,
+
+  on_construct = molder_on_construct,
+  on_rightclick = molder_on_rightclick,
+  on_timer = molder_on_timer,
 })
