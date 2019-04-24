7f9d512732070e931af7ab0d1b32110e1c7243a8
Corey Powell <mistdragon100@gmail.com>
2019-04-23 20:07:44 -0500

    yatm_rails: Stubbed files

--- yatm_rails/init.lua
+++ yatm_rails/init.lua
@@ -4,4 +4,10 @@
 yatm_rails = rawget(_G, "yatm_rails") or {}
 yatm_rails.modpath = minetest.get_modpath(minetest.get_current_modname())
 
---dofile(yatm_rails.modpath .. "/nodes.lua")
+dofile(yatm_rails.modpath .. "/api.lua")
+
+dofile(yatm_rails.modpath .. "/entities.lua")
+dofile(yatm_rails.modpath .. "/nodes.lua")
+dofile(yatm_rails.modpath .. "/items.lua")
+
+dofile(yatm_rails.modpath .. "/tests.lua")
