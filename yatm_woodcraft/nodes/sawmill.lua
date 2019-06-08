ec3befda35880fe9e511c0dea218629cbb0eb70f
Corey Powell <mistdragon100@gmail.com>
2019-06-08 18:16:22 -0500

    yatm_woodcraft: Added sawmill

---
+++ yatm_woodcraft/nodes/sawmill.lua
@@ -0,0 +1,25 @@
+local function sawmill_on_construct ()
+end
+
+minetest.register_node("yatm_woodcraft:sawmill", {
+  description = "Sawmill",
+
+  groups = {
+    cracky = 1,
+    sawmill = 1,
+  },
+
+  tiles = {
+    "yatm_sawmill_top.png",
+    "yatm_sawmill_bottom.png",
+    "yatm_sawmill_side.png",
+    "yatm_sawmill_side.png",
+    "yatm_sawmill_front.png",
+    "yatm_sawmill_back.png",
+  },
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+
+  on_construct = sawmill_on_construct,
+})
