f5dea0ef14801e00407cf0a0ff240cc7224bc3cd
Corey Powell <mistdragon100@gmail.com>
2019-06-11 23:57:23 -0500

    yatm_oku: RV32I modelling
    Uggggggh

--- yatm_oku/oku.lua
+++ yatm_oku/oku.lua
@@ -15,52 +15,111 @@ local ffi = assert(yatm_oku.ffi)
 
 ffi.cdef[[
 union yatm_oku_register32 {
-  int8   i8v[4];
-  uint8  u8v[4];
-  int16  i16v[2];
-  uint16 u16v[2];
-  int32  i32v[1];
-  uint32 u32v[1];
+  int8_t   i8v[4];
+  uint8_t  u8v[4];
+  int16_t  i16v[2];
+  uint16_t u16v[2];
+  int32_t  i32v[1];
+  uint32_t u32v[1];
   float  fv[1];
-  int32  i;
-  uint32 u;
+  int32_t  i;
+  uint32_t u;
   float  f;
-}
+};
+]]
 
-struct yatm_oku_registers {
-  yatm_oku_register32 x0;
-  yatm_oku_register32 x1;
-  yatm_oku_register32 x2;
-  yatm_oku_register32 x3;
-  yatm_oku_register32 x4;
-  yatm_oku_register32 x5;
-  yatm_oku_register32 x6;
-  yatm_oku_register32 x7;
-  yatm_oku_register32 x8;
-  yatm_oku_register32 x9;
-  yatm_oku_register32 x10;
-  yatm_oku_register32 x11;
-  yatm_oku_register32 x12;
-  yatm_oku_register32 x13;
-  yatm_oku_register32 x14;
-  yatm_oku_register32 x15;
-  yatm_oku_register32 x16;
-  yatm_oku_register32 x17;
-  yatm_oku_register32 x18;
-  yatm_oku_register32 x19;
-  yatm_oku_register32 x20;
-  yatm_oku_register32 x21;
-  yatm_oku_register32 x22;
-  yatm_oku_register32 x23;
-  yatm_oku_register32 x24;
-  yatm_oku_register32 x25;
-  yatm_oku_register32 x26;
-  yatm_oku_register32 x27;
-  yatm_oku_register32 x28;
-  yatm_oku_register32 x29;
-  yatm_oku_register32 x30;
-  yatm_oku_register32 x31;
-  uint32 pc;
+ffi.cdef[[
+struct yatm_oku_registers32 {
+  union yatm_oku_register32 x0;
+  union yatm_oku_register32 x1;
+  union yatm_oku_register32 x2;
+  union yatm_oku_register32 x3;
+  union yatm_oku_register32 x4;
+  union yatm_oku_register32 x5;
+  union yatm_oku_register32 x6;
+  union yatm_oku_register32 x7;
+  union yatm_oku_register32 x8;
+  union yatm_oku_register32 x9;
+  union yatm_oku_register32 x10;
+  union yatm_oku_register32 x11;
+  union yatm_oku_register32 x12;
+  union yatm_oku_register32 x13;
+  union yatm_oku_register32 x14;
+  union yatm_oku_register32 x15;
+  union yatm_oku_register32 x16;
+  union yatm_oku_register32 x17;
+  union yatm_oku_register32 x18;
+  union yatm_oku_register32 x19;
+  union yatm_oku_register32 x20;
+  union yatm_oku_register32 x21;
+  union yatm_oku_register32 x22;
+  union yatm_oku_register32 x23;
+  union yatm_oku_register32 x24;
+  union yatm_oku_register32 x25;
+  union yatm_oku_register32 x26;
+  union yatm_oku_register32 x27;
+  union yatm_oku_register32 x28;
+  union yatm_oku_register32 x29;
+  union yatm_oku_register32 x30;
+  union yatm_oku_register32 x31;
+  uint32_t pc;
+};
+]]
+
+ffi.cdef[[
+union yatm_oku_rv32i_isa {
+  int32_t value;
+  struct r {
+    int8_t opcode : 7;
+    int8_t rd : 5;
+    int8_t funct3 : 3;
+    int8_t rs1 : 5;
+    int8_t rs2 : 5;
+    int8_t funct7 : 7;
+  };
+  struct i {
+    int8_t opcode : 7;
+    int8_t rd : 5;
+    int8_t funct3 : 3;
+    int8_t rs1 : 5;
+    int16_t imm : 12;
+  };
+  struct s {
+    int8_t opcode : 7;
+    int8_t imm0 : 5;
+    int8_t funct3 : 3;
+    int8_t rs1 : 5;
+    int8_t rs2 : 5;
+    int8_t imm1 : 7;
+  };
+  struct u {
+    int8_t opcode : 7;
+    int8_t rd : 5;
+    int32_t imm : 20;
+  };
+  struct b {
+    int8_t opcode : 7;
+    int8_t imm0 : 1;
+    int8_t imm1 : 4;
+    int8_t funct3 : 3;
+    int8_t rs1 : 5;
+    int8_t rs2 : 5;
+    int8_t imm2 : 6;
+    int8_t imm3 : 1;
+  };
+  struct j {
+    int8_t opcode : 7;
+    int8_t rd : 5;
+    union {
+      int32_t offset : 20;
+      struct {
+        int8_t imm0 : 8;
+        int8_t imm1 : 1;
+        int16_t imm2 : 10;
+        int8_t imm3 : 1;
+      };
+    };
+  };
 };
 ]]
 
@@ -68,17 +127,8 @@ local OKU = yatm_oku.OKU
 local m = OKU.instance_class
 
 function m:initialize()
-  self.registers = {
-    pc = 0,
-  }
-
-  for i = 0,31 do
-    self.registers["x" .. i] = 0
-  end
-
-  --self.memory_size = 0x100000 -- Roughly 1mb
-  self.memory_size = 0x40000 -- Roughly 256kb
-  self.memory = yatm_oku.OKU.BinaryMemory:new(self.memory_size)
+  self.registers = ffi.new("yatm_oku_registers32")
+  self.memory = yatm_oku.OKU.Memory:new(0x40000 --[[ Roughly 256kb ]])
 end
 
 function m:step()
--- yatm_oku/tests.lua
+++ yatm_oku/tests.lua
@@ -1,2 +1,2 @@
-dofile(yatm_oku.modpath .. "/tests/oku/memory/binary_memory_test.lua")
+dofile(yatm_oku.modpath .. "/tests/oku/memory_test.lua")
 dofile(yatm_oku.modpath .. "/tests/oku_test.lua")
