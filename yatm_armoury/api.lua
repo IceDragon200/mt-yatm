a4319758c209ca76129588ff7415b4d666d18385
Corey Powell <mistdragon100@gmail.com>
2019-06-15 19:41:27 -0500

    yatm_armoury: is_stack_ammunition ensures that the ammunition group is present

--- yatm_armoury/api.lua
+++ yatm_armoury/api.lua
@@ -1,10 +1,14 @@
 --
 -- Determines if the given stack is a form of ammunition
+--
+-- @spec yatm_armoury.is_stack_ammunition(ItemStack.t) :: boolean
 function yatm_armoury.is_stack_ammunition(stack)
   if stack then
     local def = stack:get_definition()
     if def then
-      return def.groups.ammunition > 0
+      if def.groups.ammunition then
+        return def.groups.ammunition > 0
+      end
     end
   end
   return false
