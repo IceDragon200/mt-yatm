fc915d8632ff0c14966d953548f7533d6b949059
Corey Powell <mistdragon100@gmail.com>
2019-06-19 01:35:28 -0500

    yatm_papercraft: Added paper textures

--- yatm_papercraft/items/cardboard.lua
+++ yatm_papercraft/items/cardboard.lua
@@ -1,4 +1,4 @@
 minetest.register_craftitem("yatm_papercraft:cardboard", {
   description = "Cardboard",
-  inventory_image = "yatm_cardboard.png",
+  inventory_image = "yatm_cardboard_plain.png",
 })
--- yatm_papercraft/items/paper.lua
+++ yatm_papercraft/items/paper.lua
@@ -1,4 +1,4 @@
 minetest.register_craftitem("yatm_papercraft:paper", {
   description = "Paper",
-  inventory_image = "yatm_paper.png",
+  inventory_image = "yatm_paper_plain.png",
 })
