3307ababcf091b9bf3e73da29707da30dfb7b120
Corey Powell <mistdragon100@gmail.com>
2019-06-24 22:15:14 -0500

    yatm_core: Removed face test

--- yatm_core/nodes.lua
+++ yatm_core/nodes.lua
@@ -1,14 +1,1 @@
-minetest.register_node("yatm_core:face_test", {
-  description = "Face Test",
-  groups = {cracky = 1},
-  tiles = {
-    "yatm_debug_6.png",
-    "yatm_debug_5.png",
-    "yatm_debug_2.png",
-    "yatm_debug_4.png",
-    "yatm_debug_1.png",
-    "yatm_debug_3.png",
-  },
-  paramtype = "light",
-  paramtype2 = "facedir",
-})
+-- Nothing here anymore
