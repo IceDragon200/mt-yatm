fe5b41a548f9d919722ce9395af7c045a2446d6d
Corey Powell <mistdragon100@gmail.com>
2019-09-23 21:40:05 -0500

    yatm_core: Added metaref_merge_fields_from_table

--- yatm_core/util/meta_ref.lua
+++ yatm_core/util/meta_ref.lua
@@ -1,3 +1,11 @@
+function yatm_core.metaref_merge_fields_from_table(meta, params)
+  local base = meta:to_table()
+  local new_fields = yatm_core.table_merge(base.fields, params)
+  base.fields = new_fields
+  meta:from_table(base)
+  return meta
+end
+
 function yatm_core.metaref_dec_float(meta, name, amount)
   amount = amount or 1
   local n = meta:get_float(name)
