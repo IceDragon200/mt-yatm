3659c67de8c3d040815064966f6d68253b6225b5
Corey Powell <mistdragon100@gmail.com>
2019-10-17 12:49:45 -0500

    yatm_reactors: Fixed up data bus

--- yatm_reactors/nodes/data_bus.lua
+++ yatm_reactors/nodes/data_bus.lua
@@ -1,3 +1,7 @@
+local data_bus_data_interface = {}
+function data_bus_data_interface:receive_pdu(pos, node, port, value)
+end
+
 for _, variant in ipairs({"hazard", "coolant", "signal"}) do
   local data_bus_yatm_network = {
     kind = "machine",
@@ -14,7 +18,7 @@ for _, variant in ipairs({"hazard", "coolant", "signal"}) do
     }
   }
 
-  yatm.devices.register_network_device(data_bus_yatm_network.states.off, {
+  yatm.devices.register_stateful_network_device({
     description = "Reactor Data Bus (" .. variant .. ")",
     groups = {cracky = 1, yatm_data_device = 1},
     drop = data_bus_yatm_network.states.off,
@@ -28,40 +32,33 @@ for _, variant in ipairs({"hazard", "coolant", "signal"}) do
     },
     paramtype = "light",
     paramtype2 = "facedir",
-    yatm_network = data_bus_yatm_network,
-  })
 
-  yatm.devices.register_network_device(data_bus_yatm_network.states.error, {
-    description = "Reactor Data Bus (" .. variant .. ")",
-    groups = {cracky = 1, yatm_data_device = 1, not_in_creative_inventory = 1},
-    drop = data_bus_yatm_network.states.off,
-    tiles = {
-      "yatm_reactor_casing.plain.png",
-      "yatm_reactor_casing.plain.png",
-      "yatm_reactor_casing.plain.png",
-      "yatm_reactor_casing.plain.png^[transformFX",
-      "yatm_reactor_casing.plain.png",
-      "yatm_reactor_" .. variant .. "_data_bus_front.error.png"
-    },
-    paramtype = "light",
-    paramtype2 = "facedir",
     yatm_network = data_bus_yatm_network,
-  })
 
-  yatm.devices.register_network_device(data_bus_yatm_network.states.on, {
-    description = "Reactor Data Bus (" .. variant .. ")",
-    groups = {cracky = 1, yatm_data_device = 1, not_in_creative_inventory = 1},
-    drop = data_bus_yatm_network.states.off,
-    tiles = {
-      "yatm_reactor_casing.plain.png",
-      "yatm_reactor_casing.plain.png",
-      "yatm_reactor_casing.plain.png",
-      "yatm_reactor_casing.plain.png^[transformFX",
-      "yatm_reactor_casing.plain.png",
-      "yatm_reactor_" .. variant .. "_data_bus_front.on.png"
+    data_network_device = {
+      type = "device",
     },
-    paramtype = "light",
-    paramtype2 = "facedir",
-    yatm_network = data_bus_yatm_network,
+    data_interface = data_bus_data_interface,
+  }, {
+    error = {
+      tiles = {
+        "yatm_reactor_casing.plain.png",
+        "yatm_reactor_casing.plain.png",
+        "yatm_reactor_casing.plain.png",
+        "yatm_reactor_casing.plain.png^[transformFX",
+        "yatm_reactor_casing.plain.png",
+        "yatm_reactor_" .. variant .. "_data_bus_front.error.png"
+      },
+    },
+    on = {
+      tiles = {
+        "yatm_reactor_casing.plain.png",
+        "yatm_reactor_casing.plain.png",
+        "yatm_reactor_casing.plain.png",
+        "yatm_reactor_casing.plain.png^[transformFX",
+        "yatm_reactor_casing.plain.png",
+        "yatm_reactor_" .. variant .. "_data_bus_front.on.png"
+      },
+    }
   })
 end
