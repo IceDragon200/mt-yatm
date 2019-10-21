36007936b5fa226881deb7c6606ddc463b36d4dd
Corey Powell <mistdragon100@gmail.com>
2019-10-21 00:18:17 -0500

    yatm_cluster_thermal: Moved api to after defining thermal cluster

--- yatm_cluster_thermal/init.lua
+++ yatm_cluster_thermal/init.lua
@@ -1,10 +1,9 @@
 yatm_cluster_thermal = rawget(_G, "yatm_cluster_thermal") or {}
 yatm_cluster_thermal.modpath = minetest.get_modpath(minetest.get_current_modname())
 
-dofile(yatm_cluster_thermal.modpath .. "/api.lua")
-
 dofile(yatm_cluster_thermal.modpath .. "/thermal_system.lua")
 dofile(yatm_cluster_thermal.modpath .. "/thermal_cluster.lua")
+dofile(yatm_cluster_thermal.modpath .. "/api.lua")
 dofile(yatm_cluster_thermal.modpath .. "/hooks.lua")
 
 dofile(yatm_cluster_thermal.modpath .. "/tests.lua")
