1c1fdee96042031cbb29108eb0e00da27b5931f5
Corey Powell <mistdragon100@gmail.com>
2019-10-25 17:23:12 -0500

    yatm_decor: Added slab and plate variants of warning stripes
    Because why not

--- yatm_decor/nodes/warning_checkers.lua
+++ yatm_decor/nodes/warning_checkers.lua
@@ -1,6 +1,16 @@
 local colors = {"white", "red", "yellow"}
 local sizes = {"8x"}
 
+local slab_nodebox = {
+  type = "fixed",
+  fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+}
+
+local plate_nodebox = {
+  type = "fixed",
+  fixed = {-0.5, -0.5, -0.5, 0.5, (2 / 16.0) - 0.5, 0.5},
+}
+
 for _,color in ipairs(colors) do
   for _,size in ipairs(sizes) do
     minetest.register_node("yatm_decor:warning_checkers_" .. size .. "_" .. color, {
@@ -12,6 +22,33 @@ for _,color in ipairs(colors) do
       place_param2 = 0,
       paramtype = "light",
       paramtype2 = "facedir",
+    })
+
+    minetest.register_node("yatm_decor:warning_checkers_slab_" .. size .. "_" .. color, {
+      description = "Warning Checkers " .. size .. " (" .. color .. ")",
+      groups = {cracky = 1},
+      tiles = {
+        "yatm_warning_checkers_" .. size .. "_" .. color .. "_15.png",
+      },
+      place_param2 = 0,
+      paramtype = "light",
+      paramtype2 = "facedir",
+
+      drawtype = "nodebox",
+      node_box = slab_nodebox,
+    })
+
+    minetest.register_node("yatm_decor:warning_checkers_plate_" .. size .. "_" .. color, {
+      description = "Warning Checkers " .. size .. " (" .. color .. ")",
+      groups = {cracky = 1},
+      tiles = {
+        "yatm_warning_checkers_" .. size .. "_" .. color .. "_15.png",
+      },
+      paramtype = "light",
+      paramtype2 = "facedir",
+
+      drawtype = "nodebox",
+      node_box = plate_nodebox,
     })
   end
 end
--- yatm_decor/nodes/warning_stripes.lua
+++ yatm_decor/nodes/warning_stripes.lua
@@ -1,6 +1,16 @@
 local colors = {"white", "red", "yellow", "fiber"}
 local sizes = {"2x", "4x", "8x"}
 
+local slab_nodebox = {
+  type = "fixed",
+  fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+}
+
+local plate_nodebox = {
+  type = "fixed",
+  fixed = {-0.5, -0.5, -0.5, 0.5, (2 / 16.0) - 0.5, 0.5},
+}
+
 for _,color in ipairs(colors) do
   for _,size in ipairs(sizes) do
     if size ~= "2x" and color == "fiber" then
@@ -8,13 +18,52 @@ for _,color in ipairs(colors) do
     else
       minetest.register_node("yatm_decor:warning_stripes_" .. size .. "_" .. color, {
         description = "Warning Stripes " .. size .. " (" .. color .. ")",
+
         groups = {cracky = 1},
+
         tiles = {
           "yatm_warning_stripes_" .. size .. "_" .. color .. "_15.png",
         },
+
         paramtype = "light",
         paramtype2 = "facedir",
+
         place_param2 = 0,
+      })
+
+
+      minetest.register_node("yatm_decor:warning_stripes_slab_" .. size .. "_" .. color, {
+        description = "Warning Stripes Slab " .. size .. " (" .. color .. ")",
+
+        groups = {cracky = 1},
+
+        tiles = {
+          "yatm_warning_stripes_" .. size .. "_" .. color .. "_15.png",
+        },
+
+        paramtype = "light",
+        paramtype2 = "facedir",
+
+        place_param2 = 0,
+
+        drawtype = "nodebox",
+        node_box = slab_nodebox,
+      })
+
+      minetest.register_node("yatm_decor:warning_stripes_plate_" .. size .. "_" .. color, {
+        description = "Warning Stripes Plate " .. size .. " (" .. color .. ")",
+
+        groups = {cracky = 1},
+
+        tiles = {
+          "yatm_warning_stripes_" .. size .. "_" .. color .. "_15.png",
+        },
+
+        paramtype = "light",
+        paramtype2 = "facedir",
+
+        drawtype = "nodebox",
+        node_box = plate_nodebox,
       })
     end
   end
