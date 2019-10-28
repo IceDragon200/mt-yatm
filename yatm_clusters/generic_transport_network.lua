78c5ab78cc5545ac144d31e346c91de47bb5b824
Corey Powell <mistdragon100@gmail.com>
2019-10-27 19:54:59 -0500

    yatm_clusters: Generic transport network now manages blocks

--- yatm_clusters/generic_transport_network.lua
+++ yatm_clusters/generic_transport_network.lua
@@ -29,10 +29,13 @@ function m:initialize(config)
   self.m_abbr = config.abbr
   self.m_description = config.description
   self.m_node_interface_name = config.node_interface_name
+
   -- this contains a list of all the members in the network indexed by their hashed position
   self.m_members = {}
   -- this contains a map of members indexed first by their main type (i.e. inserter, extractor, transporter) and then by their hash position
   self.m_members_by_type = {}
+  self.m_block_members = {}
+
   self.m_networks = {}
   self.m_queue = {}
   self.m_counter = 0
@@ -68,36 +71,42 @@ end
 function m:update_member(pos, node, is_register)
   assert(pos, "expected a position")
   assert(node, "expected a node")
+
   if is_register then
     print(self.m_description, "update_member/3", "registering", minetest.pos_to_string(pos), node.name)
   else
     print(self.m_description, "update_member/3", "updating registration", minetest.pos_to_string(pos), node.name)
   end
-  local hash = minetest.hash_node_position(pos)
+
+  local node_id = minetest.hash_node_position(pos)
   local nodedef = minetest.registered_nodes[node.name]
   local interface = assert(nodedef[self.m_node_interface_name])
   local device_type = assert(interface.type)
   local device_subtype = interface.subtype
-  local old_record = self.m_members[hash]
+  local old_record = self.m_members[node_id]
   local old_network_id = nil
+
   if old_record then
     if is_register then
       print(self.m_description, "WARN", "duplicate registration attempted", minetest.pos_to_string(pos), node.name)
     else
       print(self.m_description, "removing old record", minetest.pos_to_string(pos), node.name)
     end
-    self.m_members_by_type[old_record.device_type][hash] = nil
+
+    self.m_members_by_type[old_record.device_type][node_id] = nil
     if yatm_core.is_table_empty(self.m_members_by_type[old_record.device_type]) then
       self.m_members_by_type[old_record.device_type] = nil
     end
+
     if old_record.network_id then
       old_network_id = old_record.network_id
+
       if self.m_networks[network_id] then
         -- invalidate the network
         local n = self.m_networks[network_id]
-        n.members[hash] = nil
+        n.members[node_id] = nil
         if n.members_by_type[old_record.device_type] then
-          n.members_by_type[old_record.device_type][hash] = nil
+          n.members_by_type[old_record.device_type][node_id] = nil
         else
           print(self.m_description, "WARN", "no members of type", network_id, old_record.device_type)
         end
@@ -108,6 +117,8 @@ function m:update_member(pos, node, is_register)
   end
 
   local record = {
+    id = node_id,
+    block_id = 0,
     pos = pos,
     name = node.name,
     device_type = device_type,
@@ -116,50 +127,94 @@ function m:update_member(pos, node, is_register)
     counter = -1,
     interface = interface,
   }
-  self.m_members[hash] = record
-  self.m_members_by_type[device_type] = self.m_members_by_type[device_type] or {}
-  self.m_members_by_type[device_type][hash] = record
+
+  self.m_members[node_id] = record
+  if not self.m_members_by_type[device_type] then
+    self.m_members_by_type[device_type] = {}
+  end
+  self.m_members_by_type[device_type][node_id] = record
+
+  local block_id = yatm.clusters:mark_node_block(pos, node)
+  record.block_id = block_id
+  if not self.m_block_members[record.block_id] then
+    self.m_block_members[record.block_id] = {}
+  end
+  self.m_block_members[record.block_id][node_id] = true
 
   -- register will cause a refresh on it's own position
   self:queue_all_adjacent(pos)
 end
 
 function m:queue_all_adjacent(pos)
-  local hash = minetest.hash_node_position(pos)
-  self.m_queue[hash] = pos
+  local node_id = minetest.hash_node_position(pos)
+
+  self.m_queue[node_id] = pos
+
   for dir,v3 in pairs(yatm_core.DIR6_TO_VEC3) do
     local new_pos = vector.add(pos, v3)
     local new_hash = minetest.hash_node_position(new_pos)
+
     self.m_queue[new_hash] = new_pos
   end
 end
 
 function m:unregister_member(pos)
   print(self.m_description, "unregister_member/1", "unregistering", minetest.pos_to_string(pos))
-  local hash = minetest.hash_node_position(pos)
+  local node_id = minetest.hash_node_position(pos)
 
-  local record = self.m_members[hash]
+  local record = self.m_members[node_id]
+
   if record then
     local device_type = record.device_type
-    self.m_members[hash] = nil
-    self.m_members_by_type[device_type][hash] = nil
+
+    self.m_members[node_id] = nil
+    self.m_members_by_type[device_type][node_id] = nil
+
     if yatm_core.is_table_empty(self.m_members_by_type[device_type]) then
       self.m_members_by_type[device_type] = nil
     end
+
     if record.network_id then
       local network = self.m_networks[record.network_id]
       if network then
-        network.members[hash] = nil
+        network.members[node_id] = nil
+
         local mbt = network.members_by_type[record.device_type]
+
         if mbt then
-          mbt[hash] = nil
+          mbt[node_id] = nil
         end
       end
     end
+
+    if record.block_id then
+      if self.m_block_members[record.block_id] then
+        self.m_block_members[record.block_id][node_id] = nil
+      end
+
+      if yatm_core.is_table_empty(self.m_block_members[record.block_id]) then
+        self.m_block_members[record.block_id] = nil
+      end
+    end
+
     -- unregister will cause a refresh on ALL positions adjacent to the current
     self:queue_all_adjacent(pos)
   else
     print(self.m_description, "unregister_member/1", "nothing was registered at that position!?", minetest.pos_to_string(pos))
+  end
+end
+
+function m:unload_block(block_id)
+  if self.m_block_members[block_id] then
+    local member_ids = self.m_block_members[block_id]
+    self.m_block_members[block_id] = {}
+
+    for member_id,_ in pairs(member_ids) do
+      local member = self.m_members[member_id]
+      if member then
+        self:unregister_member(member.pos)
+      end
+    end
   end
 end
 
@@ -188,8 +243,10 @@ function m:get_connected_pos(pos, node, nodedef, to_visit)
     local npos = vector.add(pos, v3)
     local neighbour_node = minetest.get_node(npos)
     local neighbour_nodedef = minetest.registered_nodes[neighbour_node.name]
+
     if neighbour_nodedef and neighbour_nodedef[self.m_node_interface_name] then
       local neighbour_interface = neighbour_nodedef[self.m_node_interface_name]
+
       if matches_color(interface.color, neighbour_interface.color) then
         if interface.type == "extractor" then
           -- Extractors cannot connect to other extractors
@@ -217,9 +274,11 @@ end
 
 function m:generate_network_id()
   local result = {self.m_abbr}
+
   for i = 1,4 do
     table.insert(result, yatm_core.random_string16(4))
   end
+
   return table.concat(result, ":")
 end
 
@@ -227,11 +286,14 @@ function m:resolve_invalid_networks(counter, delta)
   if not yatm_core.is_table_empty(self.m_invalid_networks) then
     local old_invalid_networks = self.m_invalid_networks
     self.m_invalid_networks = {}
+
     for network_id,_ in pairs(old_invalid_networks) do
       local network = self.m_networks[network_id]
+
       if network then
         for member_hash,member_entry in pairs(network.members) do
           local node = minetest.get_node(member_entry.pos)
+
           if node.name ~= member_entry.name then
             print(self.m_description, "expected", member_entry.name, "got", node.name, "unregistrering for refresh")
             self:unregister_member(member_entry.pos)
@@ -249,8 +311,10 @@ function m:resolve_queue(counter, _delta)
 
     for hash,pos in pairs(queue) do
       local should_continue = true
+
       if self.m_members[hash] then
         local member = self.m_members[hash]
+
         if member.counter and member.counter == counter then
           -- No need to scan this again, it was already encountered and refreshed this round
           should_continue = false
@@ -271,8 +335,10 @@ function m:resolve_queue(counter, _delta)
 
           for _,opos in ipairs(old_to_visit) do
             local ohash = minetest.hash_node_position(opos)
+
             if not visited[ohash] then
               visited[ohash] = true
+
               local node = minetest.get_node(opos)
               local nodedef = minetest.registered_nodes[node.name]
 
@@ -281,6 +347,7 @@ function m:resolve_queue(counter, _delta)
                   pos = opos,
                   type = nodedef[self.m_node_interface_name].type,
                 }
+
                 self:get_connected_pos(opos, node, nodedef, to_visit)
               end
             end
@@ -294,26 +361,33 @@ function m:resolve_queue(counter, _delta)
             members = {},
             members_by_type = {},
           }
+
           for ohash,omember in pairs(members) do
             local entry = self.m_members[ohash]
+
             if entry then
               entry.counter = counter
               -- If it has an old network_id
               if entry.network_id then
                 local n = self.m_networks[entry.network_id]
+
                 if n then
                   -- Remove it from the old network
                   print(self.m_description, "WARN", "node still exists in a network", minetest.pos_to_string(entry.pos), entry.network_id)
+
                   n.members[ohash] = nil
                   local mbt = n.members_by_type[entry.device_type]
+
                   if mbt then
                     mbt[ohash] = nil
                   end
+
                   if yatm_core.is_table_empty(mbt) then
                     n.members_by_type[entry.device_type] = nil
                   end
                 end
               end
+
               entry.network_id = network_id
               network.members[ohash] = entry
               network.members_by_type[entry.device_type] = network.members_by_type[entry.device_type] or {}
@@ -338,6 +412,7 @@ function m:update_networks(counter, delta)
       self.m_need_network_gc = true
     else
       network.wait = (network.wait or 0) - delta
+
       while network.wait <= 0 do
         network.wait = network.wait + 0.25
         self:update_network(network, counter, delta)
@@ -349,10 +424,13 @@ end
 function m:gc_networks(counter, delta)
   print(self.m_description, "gc_networks/0", "running garbage collection")
   local keys = yatm_core.table_keys(self.m_networks)
+
   for _,network_id in ipairs(keys) do
     local network = self.m_networks[network_id]
+
     if yatm_core.is_table_empty(network.members) then
       self.m_networks[network_id] = nil
+
       print(self.m_description, "Removed empty network", network_id)
     end
   end
@@ -360,13 +438,16 @@ end
 
 function m:update(delta)
   local counter = self.m_counter
+
   self:resolve_invalid_networks(counter, delta)
   self:resolve_queue(counter, delta)
   self:update_networks(counter, delta)
+
   if self.m_need_network_gc then
     self.m_need_network_gc = false
     self:gc_networks(counter, delta)
   end
+
   self.m_counter = counter + 1
 end
 
