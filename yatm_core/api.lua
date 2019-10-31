fc6a8d1f8f422a53d0d2635662b47c3dc0ce0bf0
Corey Powell <mistdragon100@gmail.com>
2019-10-30 22:25:48 -0500

    yatm_core,yatm_mesecon_locks: Added yatm.register_stateful_node
    A helper for creating nodes that have multiple states, but share a lot of data across defintions

--- yatm_core/api.lua
+++ yatm_core/api.lua
@@ -4,3 +4,10 @@
 
 ]]
 yatm.Luna = assert(yatm_core.Luna)
+
+function yatm.register_stateful_node(basename, base, states)
+  for name, changes in pairs(states) do
+    local nodedef = yatm_core.table_merge(base, changes)
+    minetest.register_node(basename .. "_" .. name, nodedef)
+  end
+end
--- yatm_mesecon_locks/nodes/card_readers.lua
+++ yatm_mesecon_locks/nodes/card_readers.lua
@@ -50,113 +50,113 @@ local function reader_on_rightclick(pos, node, clicker, itemstack, pointed_thing
   end
 end
 
-minetest.register_node("yatm_mesecon_locks:mesecon_card_reader_off", {
-  description = "Card Reader (Mesecons)",
-
-  drop = "yatm_mesecon_locks:mesecon_card_reader_off",
-
-  groups = {
-    cracky = 1,
-    mesecon_needs_receiver = 1,
-  },
-
-  sunlight_propagates = true,
-  is_ground_content = false,
-
-  sounds = default.node_sound_metal_defaults(),
-
-  paramtype = "light",
-  paramtype2 = "facedir",
-
-  tiles = {
-    "yatm_card_reader_reader.side.png",
-    "yatm_card_reader_reader.side.png",
-    "yatm_card_reader_reader.side.png",
-    "yatm_card_reader_reader.side.png",
-    "yatm_card_reader_common.back.off.png",
-    "yatm_card_reader_reader.front.off.png",
-  },
-  drawtype = "nodebox",
-  node_box = reader_node_box,
-
-  on_construct = card_reader_on_construct,
-
-  on_rotate = mesecon.buttonlike_onrotate,
-  on_blast = mesecon.on_blastnode,
-
-  mesecon = {
-    receptor = {
-      state = mesecon.state.on,
-      rules = mesecon.rules.buttonlike_get,
-    }
-  },
-
-  on_rightclick = reader_on_rightclick,
-  on_access_card_inserted = function (pos, node, access_card)
-    node.name = "yatm_mesecon_locks:mesecon_card_reader_on"
-    minetest.swap_node(pos, node)
-    mesecon.receptor_on(pos, mesecon.rules.buttonlike_get(node))
-  end,
-})
-
-minetest.register_node("yatm_mesecon_locks:mesecon_card_reader_on", {
-  description = "Card Reader (Mesecons)",
-
-  drop = "yatm_mesecon_locks:mesecon_card_reader_off",
-
-  groups = {
-    cracky = 1,
-    mesecon_needs_receiver = 1,
-    not_in_creative_inventory = 1,
-  },
-
-  sunlight_propagates = true,
-  is_ground_content = false,
-
-  sounds = default.node_sound_metal_defaults(),
-
-  paramtype = "light",
-  paramtype2 = "facedir",
-
-  tiles = {
-    "yatm_card_reader_reader.side.png",
-    "yatm_card_reader_reader.side.png",
-    "yatm_card_reader_reader.side.png",
-    "yatm_card_reader_reader.side.png",
-    "yatm_card_reader_common.back.on.png",
-    "yatm_card_reader_reader.front.on.png",
-  },
-  drawtype = "nodebox",
-  node_box = reader_node_box,
-
-  on_construct = card_reader_on_construct,
-
-  on_rotate = mesecon.buttonlike_onrotate,
-  on_blast = mesecon.on_blastnode,
-
-  mesecon = {
-    receptor = {
-      state = mesecon.state.on,
-      rules = mesecon.rules.buttonlike_get,
-    }
-  },
-
-  on_rightclick = reader_on_rightclick,
-  on_access_card_removed = function (pos, node, access_card)
-    node.name = "yatm_mesecon_locks:mesecon_card_reader_off"
-    minetest.swap_node(pos, node)
-    mesecon.receptor_off(pos, mesecon.rules.buttonlike_get(node))
-  end,
-
-  on_dig = function (pos, node, digger)
-    local meta = minetest.get_meta(pos)
-    local inv = meta:get_inventory()
-    local access_card = inv:get_stack("access_card_slot", 1)
-    if not access_card:is_empty() then
-      return false
-    end
-    return minetest.node_dig(pos, node, digger)
+local function reader_on_dig(pos, node, digger)
+  local meta = minetest.get_meta(pos)
+  local inv = meta:get_inventory()
+  local access_card = inv:get_stack("access_card_slot", 1)
+  if not access_card:is_empty() then
+    return false
   end
+  return minetest.node_dig(pos, node, digger)
+end
+
+yatm.register_stateful_node("yatm_mesecon_locks:mesecon_card_reader", {
+  description = "Card Reader (Mesecons)",
+
+  drop = "yatm_mesecon_locks:mesecon_card_reader_off",
+
+  groups = {
+    cracky = 1,
+    mesecon_needs_receiver = 1,
+  },
+
+  sunlight_propagates = true,
+  is_ground_content = false,
+
+  sounds = default.node_sound_metal_defaults(),
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+
+  drawtype = "nodebox",
+  node_box = reader_node_box,
+
+  on_construct = card_reader_on_construct,
+
+  on_rotate = mesecon.buttonlike_onrotate,
+  on_blast = mesecon.on_blastnode,
+
+  mesecon = {
+    receptor = {
+      state = mesecon.state.on,
+      rules = mesecon.rules.buttonlike_get,
+    }
+  },
+
+  on_rightclick = reader_on_rightclick,
+
+  on_dig = reader_on_dig,
+}, {
+  off = {
+    tiles = {
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_common.back.off.png",
+      "yatm_card_reader_reader.front.off.png",
+    },
+
+    on_access_card_inserted = function (pos, node, access_card)
+      node.name = "yatm_mesecon_locks:mesecon_card_reader_on"
+      minetest.swap_node(pos, node)
+      mesecon.receptor_on(pos, mesecon.rules.buttonlike_get(node))
+    end,
+  },
+  on = {
+    groups = {
+      cracky = 1,
+      mesecon_needs_receiver = 1,
+      not_in_creative_inventory = 1,
+    },
+
+    tiles = {
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_common.back.on.png",
+      "yatm_card_reader_reader.front.on.png",
+    },
+
+    on_access_card_removed = function (pos, node, access_card)
+      node.name = "yatm_mesecon_locks:mesecon_card_reader_off"
+      minetest.swap_node(pos, node)
+      mesecon.receptor_off(pos, mesecon.rules.buttonlike_get(node))
+    end,
+  },
+  error = {
+    groups = {
+      cracky = 1,
+      mesecon_needs_receiver = 1,
+      not_in_creative_inventory = 1,
+    },
+
+    tiles = {
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_reader.side.png",
+      "yatm_card_reader_common.back.error.png",
+      "yatm_card_reader_reader.front.error.png",
+    },
+
+    on_access_card_removed = function (pos, node, access_card)
+      node.name = "yatm_mesecon_locks:mesecon_card_reader_off"
+      minetest.swap_node(pos, node)
+      mesecon.receptor_off(pos, mesecon.rules.buttonlike_get(node))
+    end,
+  }
 })
 
 if yatm_data_network then
@@ -186,7 +186,7 @@ if yatm_data_network then
     data_network:remove_node(pos, node)
   end
 
-  minetest.register_node("yatm_mesecon_locks:data_card_reader_off", {
+  yatm.register_stateful_node("yatm_mesecon_locks:data_card_reader", {
     description = "Card Reader (DATA)",
 
     drop = "yatm_mesecon_locks:data_card_reader_off",
@@ -204,14 +204,6 @@ if yatm_data_network then
     paramtype = "light",
     paramtype2 = "facedir",
 
-    tiles = {
-      "yatm_card_reader_reader.side.png",
-      "yatm_card_reader_reader.side.png",
-      "yatm_card_reader_reader.side.png",
-      "yatm_card_reader_reader.side.png",
-      "yatm_card_reader_data.back.off.png",
-      "yatm_card_reader_reader.data.front.off.png",
-    },
     drawtype = "nodebox",
     node_box = reader_node_box,
 
@@ -226,56 +218,67 @@ if yatm_data_network then
     after_destruct = data_card_reader_after_destruct,
 
     on_rightclick = reader_on_rightclick,
-    on_access_card_inserted = function (pos, node, access_card)
-      node.name = "yatm_mesecon_locks:data_card_reader_on"
-      minetest.swap_node(pos, node)
-    end,
-  })
 
-  minetest.register_node("yatm_mesecon_locks:data_card_reader_on", {
-    description = "Card Reader (DATA)",
+    on_dig = reader_on_dig,
+  }, {
+    off = {
+      tiles = {
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_data.back.off.png",
+        "yatm_card_reader_reader.data.front.off.png",
+      },
 
-    drop = "yatm_mesecon_locks:data_card_reader_off",
-
-    groups = {
-      cracky = 1,
-      yatm_data_device = 1,
-      not_in_creative_inventory = 1,
+      on_access_card_inserted = function (pos, node, access_card)
+        node.name = "yatm_mesecon_locks:data_card_reader_on"
+        minetest.swap_node(pos, node)
+      end,
     },
 
-    sunlight_propagates = true,
-    is_ground_content = false,
+    on = {
+      groups = {
+        cracky = 1,
+        yatm_data_device = 1,
+        not_in_creative_inventory = 1,
+      },
 
-    sounds = default.node_sound_metal_defaults(),
+      tiles = {
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_data.back.on.png",
+        "yatm_card_reader_reader.data.front.on.png",
+      },
 
-    paramtype = "light",
-    paramtype2 = "facedir",
-
-    tiles = {
-      "yatm_card_reader_reader.side.png",
-      "yatm_card_reader_reader.side.png",
-      "yatm_card_reader_reader.side.png",
-      "yatm_card_reader_reader.side.png",
-      "yatm_card_reader_data.back.on.png",
-      "yatm_card_reader_reader.data.front.on.png",
+      on_access_card_removed = function (pos, node, access_card)
+        node.name = "yatm_mesecon_locks:data_card_reader_off"
+        minetest.swap_node(pos, node)
+      end,
     },
-    drawtype = "nodebox",
-    node_box = reader_node_box,
 
-    data_network_device = {
-      type = "device",
-    },
-    data_interface = data_interface,
+    error = {
+      groups = {
+        cracky = 1,
+        yatm_data_device = 1,
+        not_in_creative_inventory = 1,
+      },
 
-    refresh_infotext = card_reader_refresh_infotext,
+      tiles = {
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_reader.side.png",
+        "yatm_card_reader_data.back.error.png",
+        "yatm_card_reader_reader.data.front.error.png",
+      },
 
-    on_construct = data_card_reader_on_construct,
-    after_destruct = data_card_reader_after_destruct,
-
-    on_rightclick = reader_on_rightclick,
-    on_access_card_removed = function (pos, node, access_card)
-      node.name = "yatm_mesecon_locks:data_card_reader_off"
-      minetest.swap_node(pos, node)
-    end,
+      on_access_card_removed = function (pos, node, access_card)
+        node.name = "yatm_mesecon_locks:data_card_reader_off"
+        minetest.swap_node(pos, node)
+      end,
+    }
   })
 end
