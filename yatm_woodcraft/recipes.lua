12915fb9b44a93d9dd5076dde68fec42ecf55543
Corey Powell <mistdragon100@gmail.com>
2019-11-07 14:48:49 -0500

    yatm_woodcraft: Added compacting recipe for packed sawdust

--- yatm_woodcraft/recipes.lua
+++ yatm_woodcraft/recipes.lua
@@ -199,3 +199,14 @@ if not stairs then
       },
       0.25)
 end
+
+if yatm_machines then
+  local compacting_registry = assert(yatm.compacting.compacting_registry)
+  local reg = compacting_registry:method("register_compacting_recipe")
+
+  reg("yatm_woodcraft:compact_saw_dust",
+      -- It requires 4x more sawdust to make a packed sawdust block
+      ItemStack("yatm_woodcraft:sawdust 36"),
+      ItemStack("yatm_woodcraft:packed_sawdust_block 1"),
+      5.0)
+end
