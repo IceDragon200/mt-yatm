d0e73309986c74f3648c327af3b94bf8611f2d9c
Corey Powell <mistdragon100@gmail.com>
2019-11-10 13:03:13 -0500

    yatm_core: Added register_stateful_{tool,craftitem}

--- yatm_core/api.lua
+++ yatm_core/api.lua
@@ -8,6 +8,23 @@ yatm.Luna = assert(yatm_core.Luna)
 function yatm.register_stateful_node(basename, base, states)
   for name, changes in pairs(states) do
     local nodedef = yatm_core.table_merge(base, changes)
+    nodedef.basename = nodedef.basename or basename
     minetest.register_node(basename .. "_" .. name, nodedef)
+  end
+end
+
+function yatm.register_stateful_tool(basename, base, states)
+  for name, changes in pairs(states) do
+    local tooldef = yatm_core.table_merge(base, changes)
+    tooldef.basename = tooldef.basename or basename
+    minetest.register_tool(basename .. "_" .. name, tooldef)
+  end
+end
+
+function yatm.register_stateful_craftitem(basename, base, states)
+  for name, changes in pairs(states) do
+    local craftitemdef = yatm_core.table_merge(base, changes)
+    craftitemdef.basename = craftitemdef.basename or basename
+    minetest.register_craftitem(basename .. "_" .. name, craftitemdef)
   end
 end
