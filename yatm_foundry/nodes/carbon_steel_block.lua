3d608c914ff231e44aaffab2ee44df9a7ef79d60
Corey Powell <mistdragon100@gmail.com>
2019-11-17 18:14:12 -0500

    yatm_foundry: New carbon steel block
    Realy just for decorative purposes

--- yatm_foundry/nodes/carbon_steel_block.lua
+++ yatm_foundry/nodes/carbon_steel_block.lua
@@ -13,3 +13,19 @@ minetest.register_node("yatm_foundry:carbon_steel_block", {
 
   sounds = default.node_sound_metal_defaults(),
 })
+
+minetest.register_node("yatm_foundry:carbon_steel_smooth_block", {
+  basename = "yatm_foundry:carbon_steel_smooth_block",
+
+  description = "Carbon Steel Smooth Block",
+
+  groups = {cracky = 1},
+
+  tiles = {
+    "yatm_carbon_steel_block_smooth.side.png",
+  },
+  paramtype = "light",
+  paramtype2 = "facedir",
+
+  sounds = default.node_sound_metal_defaults(),
+})
