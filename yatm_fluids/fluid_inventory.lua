22b83770949d531a609a1f7aa134d3580be3fb70
Corey Powell <mistdragon100@gmail.com>
2019-12-14 18:18:02 -0500

    yatm_fluids: Rewrote FluidInventory a bit
    It now mimics item inventories a tad bit with lists

--- yatm_fluids/fluid_inventory.lua
+++ yatm_fluids/fluid_inventory.lua
@@ -8,79 +8,154 @@ local FluidStack = yatm_fluids.FluidStack
 local FluidInventory = yatm_core.Class:extends("FluidInventory")
 local ic = FluidInventory.instance_class
 
-function ic:initialize(name, size, max_capacity)
+function ic:initialize(name)
   self.name = name
-  self.size = size
-  self.max_capacity = max_capacity
-  self.entries = {}
-
-  local inventory = self.m_inventories[name]
-
-  for i = 1,size do
-    inventory.entries[i] = FluidStack.new_empty()
-  end
+  self.m_lists = {}
 end
 
-function ic:is_empty()
-  for _, slot_stack in pairs(self.entries) do
-    if not FluidStack.is_empty(slot_stack) then
-      return false
+function ic:set_size(list_name, size)
+  if size > 0 then
+    local old_list = self.m_lists[list_name]
+    self.m_lists[list_name] = {
+      size = size,
+      max_stack_size = 0,
+      entries = {}
+    }
+
+    local list = self.m_lists[list_name]
+
+    if old_list then
+      list.max_stack_size = old_list.max_stack_size
+      for i = 1,list.size do
+        list.entries[i] = old_list.entries[i] or FluidStack.new_empty()
+      end
+    else
+      for i = 1,list.size do
+        list.entries[i] = FluidStack.new_empty()
+      end
+    end
+  end
+  return self
+end
+
+function ic:set_max_stack_size(list_name, max_stack_size)
+  local list = self.m_lists[list_name]
+  if list then
+    list.max_stack_size = max_stack_size
+  end
+  return self
+end
+
+function ic:is_empty(list_name)
+  local list = self.m_lists[list_name]
+  if list then
+    for _, slot_stack in pairs(list.entries) do
+      if not FluidStack.is_empty(slot_stack) then
+        return false
+      end
     end
   end
   return true
 end
 
-function ic:get_fluid_stack(index)
-  return self.entries[index]
+function ic:get_fluid_stack(list_name, index)
+  local list = self.m_lists[list_name]
+  if list then
+    return list.entries[index]
+  else
+    return nil
+  end
 end
 
-function ic:set_fluid_stack(index, fluid_stack)
-  if fluid_stack then
-    self.entries[index] = FluidStack.copy(fluid_stack)
-    if self.max_capacity > 0 then
-      self.entries[index].amount = math.min(self.entries[index].amount, self.max_capacity)
+function ic:set_fluid_stack(list_name, index, fluid_stack)
+  local list = self.m_lists[list_name]
+  if list then
+    if fluid_stack then
+      list.entries[index] = FluidStack.copy(fluid_stack)
+      if list.max_stack_size > 0 then
+        list.entries[index].amount = math.min(list.entries[index].amount, list.max_stack_size)
+      end
+    else
+      list.entries[index] = FluidStack.new_empty()
     end
-  else
-    self.entries[index] = FluidStack.new_empty()
   end
   return self
 end
 
-function ic:contains_fluid_stack(fluid_stack)
+function ic:contains_fluid_stack(list_name, fluid_stack)
   -- TODO: should this support 'amount' rollover?
-  --       that is, if the given fluid stack has an amount that exceeds the max_capacity
+  --       that is, if the given fluid stack has an amount that exceeds the max_stack_size
   --       should it look for other slots until that amount is 0?
-  for _, slot_stack in pairs(self.entries) do
-    if FluidStack.same_fluid(slot_stack, fluid_stack) then
-      return slot_stack.amount >= fluid_stack.amount
+  local list = self.m_lists[list_name]
+  if list then
+    for _, slot_stack in pairs(list.entries) do
+      if FluidStack.same_fluid(slot_stack, fluid_stack) then
+        return slot_stack.amount >= fluid_stack.amount
+      end
     end
   end
   return false
 end
 
-function ic:add_fluid_stack(fluid_stack)
+function ic:add_fluid_stack(list_name, fluid_stack)
   assert(fluid_stack, "expected a fluid stack")
 
-  local remaining_stack = FluidStack.copy(fluid_stack)
+  local list = self.m_lists[list_name]
+  if list then
+    local remaining_stack = FluidStack.copy(fluid_stack)
 
-  for i = 1,self.size do
-    if remaining_stack.amount <= 0 then
-      break
-    end
-    local slot_stack = self.entries[i]
-    if FluidStack.is_empty(slot_stack) then
-      self.entries[i] = FluidStack.merge({}, remaining_stack)
-    elseif FluidStack.same_fluid(slot_stack, remaining_stack) then
-      local new_stack = FluidStack.merge(slot_stack, remaining_stack)
-      if self.max_capacity > 0 then
-        local new_amount = math.min(new_stack.amount, self.max_capacity)
-        local used_amount = new_amount - slot_stack.amount
-        remaining_stack.amount = remaining_stack.amount - used_amount
+    for i = 1,list.size do
+      if remaining_stack.amount <= 0 then
+        break
       end
-      self.entries[i] = new_stack
+      local slot_stack = list.entries[i]
+      if FluidStack.is_empty(slot_stack) then
+        list.entries[i] = FluidStack.merge({}, remaining_stack)
+      elseif FluidStack.same_fluid(slot_stack, remaining_stack) then
+        local new_stack = FluidStack.merge(slot_stack, remaining_stack)
+        if list.max_stack_size > 0 then
+          local new_amount = math.min(new_stack.amount, list.max_stack_size)
+          local used_amount = new_amount - slot_stack.amount
+          remaining_stack.amount = remaining_stack.amount - used_amount
+        end
+        list.entries[i] = new_stack
+      end
     end
   end
   return self
+end
+
+function ic:to_table()
+  return {
+    name = self.name,
+    lists = self.m_lists,
+  }
+end
+
+function ic:from_table(data_dump)
+  self.name = data_dump.name
+  self.m_lists = data_dump.lists
+  return self
+end
+
+function ic:deserialize(blob)
+  local dumped_data = minetest.deserialize(blob)
+  return self:from_table(dumped_data)
+end
+
+function ic:serialize()
+  return minetest.serialize(self:to_table())
+end
+
+function ic:deserialize_list(list_name, blob)
+  local dumped_data = minetest.deserialize(blob)
+  self.m_lists[list_name] = dumped_data
+  return self
+end
+
+function ic:serialize_list(list_name)
+  local list = assert(self.m_lists[list_name], "expected list to exist")
+  return minetest.serialize(list)
 end
 
 --
@@ -98,17 +173,18 @@ end
 -- :create_fluid_inventory(string, integer, integer)
 --   Name is a `mod_name:inventory_name` string that identifies the inventory
 --   size is how many slots are available in the inventory
---   max_capacity - is how much fluid can be stored in any one cell
+--   max_stack_size - is how much fluid can be stored in any one cell
 --                  unlike items that have a stack_size, fluids do not enforce a limit.
 --                  A max capacity of `0` will uncap the limit
-function ic:create_fluid_inventory(name, size, max_capacity)
+function ic:create_fluid_inventory(name, size, max_stack_size)
   assert(name, "expected a name")
   assert(size, "expected a size")
-  assert(max_capacity, "expected a max capacity")
+  assert(max_stack_size, "expected a max capacity")
   if self.m_inventories[name] then
     error("fluid inventory `" .. name .. "` already exists")
   end
-  self.m_inventories[name] = FluidInventory:new(name, size, max_capacity)
+  self.m_inventories[name] = FluidInventory:new(name, size, max_stack_size)
+  return self.m_inventories[name]
 end
 
 function ic:get_fluid_inventory(name)
@@ -118,6 +194,22 @@ end
 function ic:destroy_fluid_inventory(name)
   self.m_inventories[name] = nil
   return self
+end
+
+function ic:fluid_inventory_to_table(name)
+  local inventory = self.m_inventories[name]
+  if inventory then
+    return inventory:dump()
+  end
+  return nil
+end
+
+function ic:fluid_inventory_from_table(name, data_dump)
+  local inventory = self.m_inventories[name]
+  if inventory then
+    return inventory:load(data_dump)
+  end
+  return nil
 end
 
 yatm_fluids.FluidInventory = FluidInventory
--- yatm_fluids/tests/fluid_inventory_test.lua
+++ yatm_fluids/tests/fluid_inventory_test.lua
@@ -16,102 +16,119 @@ end)
 
 case:describe("is_empty/0", function (t2)
   t2:test("reports if a fluid inventory is empty", function (t3)
-    local fi = FluidInventory:new("yatm_fluids:test_inventory", 256, 16000)
+    local fi = FluidInventory:new("yatm_fluids:test_inventory")
+
+    fi:set_size("main", 256)
+    fi:set_max_stack_size("main", 16000)
 
     -- new inventorys should be empty
-    t3:assert(fi:is_empty())
+    t3:assert(fi:is_empty("main"))
   end)
 
   t2:test("will report false if the inventory contains fluids", function (t3)
-    local fi = FluidInventory:new("yatm_fluids:test_inventory", 256, 16000)
+    local fi = FluidInventory:new("yatm_fluids:test_inventory")
+    fi:set_size("main", 256)
+    fi:set_max_stack_size("main", 16000)
 
-    t3:assert(fi:is_empty())
+    t3:assert(fi:is_empty("main"))
 
-    fi:add_fluid_stack(FluidStack.new("default:water", 100))
+    fi:add_fluid_stack("main", FluidStack.new("default:water", 100))
 
-    t3:refute(fi:is_empty())
+    t3:refute(fi:is_empty("main"))
   end)
 end)
 
 case:describe("set_fluid_stack/2", function (t2)
   t2:test("set fluid stack at specified index", function (t3)
-    local fi = FluidInventory:new("yatm_fluids:test_inventory", 256, 16000)
+    local fi = FluidInventory:new("yatm_fluids:test_inventory")
+    fi:set_size("main", 256)
+    fi:set_max_stack_size("main", 16000)
 
-    fi:set_fluid_stack(1, FluidStack.new("default:water", 100))
-    local fs = fi:get_fluid_stack(1)
+    fi:set_fluid_stack("main", 1, FluidStack.new("default:water", 100))
+    local fs = fi:get_fluid_stack("main", 1)
 
     t3:assert_eq("default:water", fs.name)
     t3:assert_eq(100, fs.amount)
   end)
 
   t2:test("setting nil will clear a specified slot", function (t3)
-    local fi = FluidInventory:new("yatm_fluids:test_inventory", 256, 16000)
+    local fi = FluidInventory:new("yatm_fluids:test_inventory")
+    fi:set_size("main", 256)
+    fi:set_max_stack_size("main", 16000)
 
-    fi:set_fluid_stack(1, FluidStack.new("default:water", 100))
-    local fs = fi:get_fluid_stack(1)
+    fi:set_fluid_stack("main", 1, FluidStack.new("default:water", 100))
+    local fs = fi:get_fluid_stack("main", 1)
 
     t3:assert_eq("default:water", fs.name)
     t3:assert_eq(100, fs.amount)
 
-    fi:set_fluid_stack(1, nil)
-    local fs = fi:get_fluid_stack(1)
-    t3:refute(fs)
+    fi:set_fluid_stack("main", 1, nil)
+    local fs = fi:get_fluid_stack("main", 1)
+    t3:refute(fs.name)
   end)
 end)
 
 case:describe("add_fluid_stack/1", function (t2)
   t2:test("will add a fluid stack fo an inventory", function (t3)
-    local fi = FluidInventory:new("yatm_fluids:test_inventory", 256, 16000)
+    local fi = FluidInventory:new("yatm_fluids:test_inventory")
+    fi:set_size("main", 256)
+    fi:set_max_stack_size("main", 16000)
 
-    fi:add_fluid_stack(FluidStack.new("default:water", 100))
+    fi:add_fluid_stack("main", FluidStack.new("default:water", 100))
 
-    local fs = fi:get_fluid_stack(1)
+    local fs = fi:get_fluid_stack("main", 1)
 
     t3:assert_eq("default:water", fs.name)
     t3:assert_eq(100, fs.amount)
   end)
 
   t2:test("will not replace an existing slot if the fluids do not match", function (t3)
-    local fi = FluidInventory:new("yatm_fluids:test_inventory", 256, 16000)
+    local fi = FluidInventory:new("yatm_fluids:test_inventory")
+    fi:set_size("main", 256)
+    fi:set_max_stack_size("main", 16000)
 
-    fi:add_fluid_stack(FluidStack.new("default:water", 100))
-    fi:add_fluid_stack(FluidStack.new("default:lava", 300))
+    fi:add_fluid_stack("main", FluidStack.new("default:water", 100))
+    fi:add_fluid_stack("main", FluidStack.new("default:lava", 300))
 
-    local fs = fi:get_fluid_stack(1)
+    local fs = fi:get_fluid_stack("main", 1)
 
     t3:assert_eq("default:water", fs.name)
     t3:assert_eq(100, fs.amount)
 
-    local fs = fi:get_fluid_stack(2)
+    local fs = fi:get_fluid_stack("main", 2)
 
     t3:assert_eq("default:lava", fs.name)
     t3:assert_eq(300, fs.amount)
   end)
 
   t2:test("will not occupy a new slot if an existing slot has enough capacity", function (t3)
-    local fi = FluidInventory:new("yatm_fluids:test_inventory", 256, 16000)
+    local fi = FluidInventory:new("yatm_fluids:test_inventory")
+    fi:set_size("main", 256)
+    fi:set_max_stack_size("main", 16000)
 
-    fi:add_fluid_stack(FluidStack.new("default:water", 100))
-    fi:add_fluid_stack(FluidStack.new("default:water", 100))
+    fi:add_fluid_stack("main", FluidStack.new("default:water", 100))
+    fi:add_fluid_stack("main", FluidStack.new("default:water", 100))
 
-    local fs = fi:get_fluid_stack(1)
+    local fs = fi:get_fluid_stack("main", 1)
 
     t3:assert_eq("default:water", fs.name)
     t3:assert_eq(200, fs.amount)
   end)
 
   t2:test("will rollover if amount exceeds available capacity", function (t3)
-    local fi = FluidInventory:new("yatm_fluids:test_inventory", 256, 16000)
+    local fi = FluidInventory:new("yatm_fluids:test_inventory")
+    fi:set_size("main", 256)
+    fi:set_max_stack_size("main", 16000)
 
-    fi:add_fluid_stack(FluidStack.new("default:water", 12000))
-    fi:add_fluid_stack(FluidStack.new("default:water", 12000))
+    fi:add_fluid_stack("main", FluidStack.new("default:water", 12000))
+    fi:add_fluid_stack("main", FluidStack.new("default:water", 12000))
 
-    local fs = fi:get_fluid_stack(1)
+    local fs = fi:get_fluid_stack("main", 1)
 
     t3:assert_eq("default:water", fs.name)
     t3:assert_eq(16000, fs.amount)
 
-    local fs = fi:get_fluid_stack(2)
+    local fs = fi:get_fluid_stack("main", 2)
     t3:assert_eq("default:water", fs.name)
     t3:assert_eq(8000, fs.amount)
   end)
