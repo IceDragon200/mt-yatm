0703091dfb5ed174127d75eb7cdbe1b0def8b50f
Corey Powell <mistdragon100@gmail.com>
2019-12-22 11:13:06 -0500

    yatm_codex: Added codex textures

--- yatm_codex/api.lua
+++ yatm_codex/api.lua
@@ -3,10 +3,29 @@ yatm.codex = yatm.codex or {}
 yatm.codex.registered_entries = {}
 yatm.codex.registered_demos = {}
 
-function yatm.codex.register_entry(name, def)
+local function default_demo_init(pos)
+  return nil
+end
 
+local function default_demo_build(pos, assigns)
+end
+
+local function default_demo_configure(pos, assigns)
+end
+
+local function default_demo_done(pos, assigns)
+end
+
+function yatm.codex.register_entry(name, def)
+  assert(def.pages, "expected to have pages")
+  yatm.codex.registered_entries[name] = def
 end
 
 function yatm.codex.register_demo(name, def)
+  def.init = def.init or default_demo_init
+  def.build = def.build or default_demo_build
+  def.configure = def.configure or default_demo_configure
+  def.done = def.done or default_demo_done
 
+  yatm.codex.registered_demos[name] = def
 end
--- yatm_codex/items/codex.lua
+++ yatm_codex/items/codex.lua
@@ -8,10 +8,25 @@ minetest.register_tool("yatm_codex:codex", {
   inventory_image = "yatm_codex.png",
 
   on_use = function (itemstack, user, pointed_thing)
-    -- when pointing at something, pull up the associated codex entry for that item
+    -- open default codex knowledge base
   end,
 
   on_place = function (itemstack, user, pointed_thing)
-    -- open default codex knowledge base
+    -- when pointing at something, pull up the associated codex entry for that item
+    local pos = pointed_thing.under
+
+    local node = minetest.get_node(pos)
+    local nodedef = minetest.registered_nodes[node.name]
+
+    local codex_entry
+    if nodedef then
+      codex_entry = nodedef.codex_entry
+    end
+
+    if codex_entry then
+      --
+    else
+      minetest.chat_send_player(user, "No CODEX entry available")
+    end
   end,
 })
