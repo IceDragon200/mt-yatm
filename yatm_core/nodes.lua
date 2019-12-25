f6a4e4b8b9050c669184f8279bdf26cd105f411c
Corey Powell <mistdragon100@gmail.com>
2019-12-25 12:07:32 -0500

    yatm_core: Added grid node

--- yatm_core/nodes.lua
+++ yatm_core/nodes.lua
@@ -18,3 +18,26 @@ minetest.register_node("yatm_core:face_debug", {
   is_ground_content = false,
 
 })
+
+minetest.register_node("yatm_core:grid_block", {
+  description = "GRID\nDummy Block",
+
+  groups = {cracky = 1},
+
+  tiles = {
+    "yatm_grid.png",
+  },
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+
+  is_ground_content = false,
+
+  drawtype = "nodebox",
+  node_box = {
+    type = "fixed",
+    fixed = {
+      {-0.5, -0.5, -0.5, 0.5, -0.49, 0.5}
+    }
+  }
+})
