81814c1042afae0bfc9de11db686aadb7730d615
Corey Powell <mistdragon100@gmail.com>
2019-12-27 17:39:33 -0500

    yatm_frames: Fixed sunlight propagation

--- yatm_frames/nodes/frame.lua
+++ yatm_frames/nodes/frame.lua
@@ -19,7 +19,7 @@ minetest.register_node("yatm_frames:frame", {
 
 -- Sticky frames act like sticky pistons dragging any connected nodes with it
 minetest.register_node("yatm_frames:frame_sticky", {
-  description = "Sticky Frame",
+  description = "Sticky Frame (All Faces)",
 
   groups = {
     cracky = 1,
@@ -46,7 +46,8 @@ minetest.register_node("yatm_frames:frame_sticky_one", {
     motor_frame_sticky_one = 1,
   },
 
-  drawtype = "nodebox",
+  sticky_faces = {yatm_core.D_SOUTH},
+
   tiles = {
     "yatm_frame_side.png",
     "yatm_frame_side.png",
@@ -56,7 +57,169 @@ minetest.register_node("yatm_frames:frame_sticky_one", {
     "yatm_frame_side_sticky.png",
   },
 
+  drawtype = "nodebox",
   node_box = {
+    type = "fixed",
+    fixed = {
+      yatm_core.Cuboid:new(0, 0, 0, 16, 16, 16):fast_node_box(),
+    },
+  },
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+})
+
+minetest.register_node("yatm_frames:frame_sticky_two", {
+  description = "Sticky Frame (Two Faces)",
+
+  groups = {
+    cracky = 1,
+    motor_frame = 1,
+    motor_frame_sticky_two = 1,
+  },
+
+  sticky_faces = {yatm_core.D_UP, yatm_core.D_SOUTH},
+
+  tiles = {
+    "yatm_frame_side_sticky.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side_sticky.png",
+  },
+
+  drawtype = "nodebox",
+  node_box = {
+    type = "fixed",
+    fixed = {
+      yatm_core.Cuboid:new(0, 0, 0, 16, 16, 16):fast_node_box(),
+    },
+  },
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+})
+
+minetest.register_node("yatm_frames:frame_sticky_three", {
+  description = "Sticky Frame (Three Faces)",
+
+  groups = {
+    cracky = 1,
+    motor_frame = 1,
+    motor_frame_sticky_three = 1,
+  },
+
+  sticky_faces = {yatm_core.D_UP, yatm_core.D_EAST, yatm_core.D_SOUTH},
+
+  tiles = {
+    "yatm_frame_side_sticky.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side_sticky.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side_sticky.png",
+  },
+
+  drawtype = "nodebox",
+  node_box = {
+    type = "fixed",
+    fixed = {
+      yatm_core.Cuboid:new(0, 0, 0, 16, 16, 16):fast_node_box(),
+    },
+  },
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+})
+
+minetest.register_node("yatm_frames:frame_sticky_four", {
+  description = "Sticky Frame (Four Faces)",
+
+  groups = {
+    cracky = 1,
+    motor_frame = 1,
+    motor_frame_sticky_four = 1,
+  },
+
+  sticky_faces = {yatm_core.D_UP, yatm_core.D_WEST, yatm_core.D_EAST, yatm_core.D_SOUTH},
+
+  tiles = {
+    "yatm_frame_side_sticky.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side_sticky.png",
+    "yatm_frame_side_sticky.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side_sticky.png",
+  },
+
+  drawtype = "nodebox",
+  node_box = {
+    type = "fixed",
+    fixed = {
+      yatm_core.Cuboid:new(0, 0, 0, 16, 16, 16):fast_node_box(),
+    },
+  },
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+})
+
+minetest.register_node("yatm_frames:frame_sticky_five", {
+  description = "Sticky Frame (Five Faces)",
+
+  groups = {
+    cracky = 1,
+    motor_frame = 1,
+    motor_frame_sticky_five = 1,
+  },
+
+  sticky_faces = {yatm_core.D_UP, yatm_core.D_DOWN, yatm_core.D_WEST, yatm_core.D_EAST, yatm_core.D_SOUTH},
+
+  tiles = {
+    "yatm_frame_side_sticky.png",
+    "yatm_frame_side_sticky.png",
+    "yatm_frame_side_sticky.png",
+    "yatm_frame_side_sticky.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side_sticky.png",
+  },
+
+  drawtype = "nodebox",
+  node_box = {
+    type = "fixed",
+    fixed = {
+      yatm_core.Cuboid:new(0, 0, 0, 16, 16, 16):fast_node_box(),
+    },
+  },
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+})
+
+minetest.register_node("yatm_frames:frame_sticky_axle", {
+  description = "Sticky Frame (Axle)",
+
+  groups = {
+    cracky = 1,
+    motor_frame = 1,
+    motor_frame_sticky_axle = 1,
+  },
+
+  sticky_faces = {yatm_core.D_NORTH, yatm_core.D_SOUTH},
+
+  tiles = {
+    "yatm_frame_side.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side.png",
+    "yatm_frame_side_sticky.png",
+    "yatm_frame_side_sticky.png",
+  },
+
+  drawtype = "nodebox",
+  node_box = {
+    type = "fixed",
     fixed = {
       yatm_core.Cuboid:new(0, 0, 0, 16, 16, 16):fast_node_box(),
     },
