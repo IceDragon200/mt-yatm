1ea3b35f03c53d4ff392f19e8e58e14cc22fe588
Corey Powell <mistdragon100@gmail.com>
2019-12-29 13:19:24 -0500

    yatm_armoury_icbm: Added 2 new guiding rings
    Single bands

--- yatm_armoury_icbm/nodes/icbm_silo.lua
+++ yatm_armoury_icbm/nodes/icbm_silo.lua
@@ -574,8 +574,21 @@ local node_box = {
   },
 }
 
+local single_node_box = {
+  type = "fixed",
+  fixed = {
+    yatm.Cuboid:new( 0,  2,  0, 15, 12,  1):fast_node_box(),
+
+    yatm.Cuboid:new(15,  2,  0,  1, 12, 15):fast_node_box(),
+
+    yatm.Cuboid:new( 1,  2, 15, 15, 12,  1):fast_node_box(),
+
+    yatm.Cuboid:new( 0,  2,  1,  1, 12, 15):fast_node_box(),
+  },
+}
+
 minetest.register_node("yatm_armoury_icbm:icbm_guiding_ring", {
-  description = "ICBM Guiding Ring",
+  description = "ICBM Guiding Ring (Double Band)",
 
   groups = {
     cracky = 1,
@@ -603,8 +616,37 @@ minetest.register_node("yatm_armoury_icbm:icbm_guiding_ring", {
   sunlight_propagates = true,
 })
 
+minetest.register_node("yatm_armoury_icbm:icbm_guiding_ring_single", {
+  description = "ICBM Guiding Ring (Single Band)",
+
+  groups = {
+    cracky = 1,
+    icbm_guiding_ring = 1,
+  },
+
+  tiles = {
+    "yatm_icbm_guiding_ring_top.png",
+    "yatm_icbm_guiding_ring_bottom.png",
+    "yatm_icbm_guiding_ring_single_side.png",
+    "yatm_icbm_guiding_ring_single_side.png",
+    "yatm_icbm_guiding_ring_single_side.png",
+    "yatm_icbm_guiding_ring_single_side.png",
+  },
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+
+  sounds = default.node_sound_metal_defaults(),
+
+  drawtype = "nodebox",
+  node_box = single_node_box,
+
+  is_ground_content = false,
+  sunlight_propagates = true,
+})
+
 minetest.register_node("yatm_armoury_icbm:icbm_guiding_ring_warning_strips", {
-  description = "ICBM Guiding Ring [Warning Strips]",
+  description = "ICBM Guiding Ring [Warning Strips] (Double Band)",
 
   groups = {
     cracky = 1,
@@ -627,6 +669,35 @@ minetest.register_node("yatm_armoury_icbm:icbm_guiding_ring_warning_strips", {
 
   drawtype = "nodebox",
   node_box = node_box,
+
+  is_ground_content = false,
+  sunlight_propagates = true,
+})
+
+minetest.register_node("yatm_armoury_icbm:icbm_guiding_ring_single_warning_strips", {
+  description = "ICBM Guiding Ring [Warning Strips] (Single Band)",
+
+  groups = {
+    cracky = 1,
+    icbm_guiding_ring = 1,
+  },
+
+  tiles = {
+    "yatm_icbm_guiding_ring_top.png",
+    "yatm_icbm_guiding_ring_bottom.png",
+    "yatm_icbm_guiding_ring_warning_single_side.png",
+    "yatm_icbm_guiding_ring_warning_single_side.png",
+    "yatm_icbm_guiding_ring_warning_single_side.png",
+    "yatm_icbm_guiding_ring_warning_single_side.png",
+  },
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+
+  sounds = default.node_sound_metal_defaults(),
+
+  drawtype = "nodebox",
+  node_box = single_node_box,
 
   is_ground_content = false,
   sunlight_propagates = true,
