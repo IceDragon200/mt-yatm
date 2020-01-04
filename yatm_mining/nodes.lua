3602be65fc46e0a4562cc9775d556d410356d34d
Corey Powell <mistdragon100@gmail.com>
2020-01-04 13:14:11 -0500

    yatm_mining: Moved quarry wall into own file

--- yatm_mining/nodes.lua
+++ yatm_mining/nodes.lua
@@ -1,2 +1,3 @@
 dofile(yatm_mining.modpath .. "/nodes/quarry.lua")
+dofile(yatm_mining.modpath .. "/nodes/quarry_wall.lua")
 dofile(yatm_mining.modpath .. "/nodes/surface_drill.lua")
---
+++ yatm_mining/nodes/quarry_wall.lua
@@ -0,0 +1,37 @@
+minetest.register_node("yatm_mining:quarry_wall", {
+  description = "Quarry Wall",
+
+  groups = {
+    snappy = 1,
+  },
+
+  tiles = {
+    "yatm_quarry_wall_top.png",
+    "yatm_quarry_wall_bottom.png",
+    "yatm_quarry_wall_side.png",
+    "yatm_quarry_wall_side.png",
+    "yatm_quarry_wall_side.png",
+    "yatm_quarry_wall_side.png",
+  },
+
+  sounds = default.node_sound_glass_defaults(),
+
+  connects_to = {
+    "yatm_mining:quarry_wall",
+  },
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+  place_param2 = 0,
+
+  drawtype = "nodebox",
+  node_box = {
+    type = "connected",
+
+    fixed = yatm_core.Cuboid:new(7, 0, 7, 2, 14, 2):fast_node_box(),
+    connect_front = yatm_core.Cuboid:new(7, 0, 0, 2, 14, 7):fast_node_box(),
+    connect_back = yatm_core.Cuboid:new(7, 0, 9, 2, 14, 7):fast_node_box(),
+    connect_left = yatm_core.Cuboid:new(0, 0, 7, 7, 14, 2):fast_node_box(),
+    connect_right = yatm_core.Cuboid:new(9, 0, 7, 7, 14, 2):fast_node_box(),
+  }
+})
