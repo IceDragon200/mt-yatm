a690b43c4af9868df4c341654368af173ef7ef69
Corey Powell <mistdragon100@gmail.com>
2020-01-04 13:14:47 -0500

    yatm_oku: Use minetest.log

--- yatm_oku/lib/oku/isa/mos_6502.lua
+++ yatm_oku/lib/oku/isa/mos_6502.lua
@@ -7,8 +7,8 @@ pcall(function ()
 end)
 
 if not oku_6502 then
-  print("oku_6502 shared object is not available, skipping implementation")
-  print("\n\nWARN: 6502 based CPUs will not be available.\n\n")
+  minetest.log("warn", "oku_6502 shared object is not available, skipping implementation")
+  minetest.log("warn", "\n\nWARN: 6502 based CPUs will not be available.\n\n")
   return
 end
 
