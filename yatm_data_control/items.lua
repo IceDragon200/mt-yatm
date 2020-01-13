b1a275560ff6247183310907348aab55b71329cf
Corey Powell <mistdragon100@gmail.com>
2020-01-12 22:53:10 -0500

    yatm_data_control: Added control switches

--- yatm_data_control/items.lua
+++ yatm_data_control/items.lua
@@ -18,6 +18,7 @@ for _,pair in ipairs(colors) do
 
     group = {
       data_control = 1,
+      data_control_momentary_button = 1,
     },
 
     dye_color = basename,
@@ -28,6 +29,26 @@ for _,pair in ipairs(colors) do
       images = {
         off = "yatm_colored_buttons_" .. basename .. ".off.png",
         on = "yatm_colored_buttons_" .. basename .. ".on.png",
+      },
+    },
+  })
+
+  minetest.register_craftitem("yatm_data_control:control_switch_" .. basename, {
+    description = "Control Switch (" .. display_name .. ")",
+
+    group = {
+      data_control = 1,
+      data_control_switch = 1,
+    },
+
+    dye_color = basename,
+    inventory_image = "yatm_colored_switches_" .. basename .. ".left.png",
+
+    data_control_spec = {
+      type = "switch2",
+      images = {
+        left = "yatm_colored_switches_" .. basename .. ".left.png",
+        right = "yatm_colored_switches_" .. basename .. ".right.png",
       },
     },
   })
--- yatm_data_control/nodes/data_control_plane.lua
+++ yatm_data_control/nodes/data_control_plane.lua
@@ -1,15 +1,23 @@
 local data_network = assert(yatm.data_network)
 
-local function render_pads(inv, assigns)
+local function render_pads(inv, meta, assigns)
   local formspec =
     "label[0.5,0.75;Pads]"
 
   local list = inv:get_list("pads")
 
+  local bw = assigns.scale
+  local bh = assigns.scale
+
+  local bd = ";" .. bw .. "," .. bh
+
+  local xoffset = (12 - assigns.width * assigns.scale) / 2
+  local yoffset = (12 - assigns.height * assigns.scale) / 2
+
   for y = 0,(assigns.height - 1) do
-    local dy = 2 + y * 2
+    local dy = yoffset + y * assigns.scale
     for x = 0,(assigns.width - 1) do
-      local dx = 2 + x * 2
+      local dx = xoffset + x * assigns.scale
 
       local i = 1 + y * assigns.width + x
 
@@ -18,25 +26,47 @@ local function render_pads(inv, assigns)
         local spec = stack:get_definition().data_control_spec
         if spec then
           if spec.type == "momentary_button" then
+            -- momentary button
             formspec =
               formspec ..
               "image_button[" .. dx .. "," .. dy ..
-                            ";2,2;" ..
+                            bd .. ";" ..
                             minetest.formspec_escape("yatm_button.base.48px.png^" .. spec.images["off"]) ..
                             ";pad_trigger_" .. i ..
                             ";" ..
                             ";true;false;" ..
-                            minetest.formspec_escape("yatm_button.base.48px.png^" ..spec.images["on"]) .. "]"
+                            minetest.formspec_escape("yatm_button.base.48px.png^" .. spec.images["on"]) .. "]"
+          elseif spec.type == "switch2" then
+            -- 2 state switch
+            local state_id = meta:get_int("pad_state_" .. i)
+            local state = "left"
+            if state_id ~= 0 then
+              state = "right"
+            end
+
+            formspec =
+              formspec ..
+              "image_button[" .. dx .. "," .. dy ..
+                            bd .. ";" ..
+                            minetest.formspec_escape("yatm_button.base.48px.png^yatm_button.base.switch.48px.png^" .. spec.images[state]) ..
+                            ";pad_toggle_" .. i ..
+                            ";" ..
+                            ";true;false;" ..
+                            minetest.formspec_escape("yatm_button.base.48px.png^yatm_button.base.switch.48px.png^" .. spec.images[state]) .. "]"
+          else
+            formspec =
+              formspec ..
+              "image[" .. dx .. "," .. dy .. bd .. ";yatm_button.base.handles.48px.png]"
           end
         else
           formspec =
             formspec ..
-            "image[" .. dx .. "," .. dy .. ";2,2;yatm_button.base.48px.png]"
+            "image[" .. dx .. "," .. dy .. bd .. ";yatm_button.base.48px.png]"
         end
       else
         formspec =
           formspec ..
-          "image[" .. dx .. "," .. dy .. ";2,2;yatm_button.base.48px.png]"
+          "image[" .. dx .. "," .. dy .. bd .. ";yatm_button.base.handles.48px.png]"
       end
     end
   end
@@ -45,16 +75,22 @@ end
 
 local function get_formspec(pos, user, assigns)
   local meta = minetest.get_meta(pos)
+  local node = minetest.get_node(pos)
+  local nodedef = minetest.registered_nodes[node.name]
   local inv = meta:get_inventory()
 
-  assigns.width = 4
-  assigns.height = 4
+  assigns.scale = 2.5
+
+  assigns.width = nodedef.control_panel.width
+  assigns.height = nodedef.control_panel.height
+  assigns.size = assigns.width * assigns.height
+  assigns.scale = 10 / assigns.width
 
   local formspec =
     "formspec_version[2]" ..
     "size[12,12]" ..
     yatm.formspec_bg_for_player(user:get_player_name(), "module") ..
-    render_pads(inv, assigns)
+    render_pads(inv, meta, assigns)
 
   return formspec
 end
@@ -63,20 +99,53 @@ local function get_formspec_name(pos)
   return "yatm_data_control:data_control_plane:" .. yatm.vector3.to_string(pos)
 end
 
-local function receive_fields(player, form_name, fields, assigns)
-  local meta = minetest.get_meta(assigns.pos)
-
-  for pad_id = 1,16 do
-    local pad_trigger = fields["pad_trigger_" .. pad_id]
-    if pad_trigger then
-      local port = meta:get_int("pad_port_" .. pad_id)
-      local value = meta:get_string("pad_value_" .. pad_id)
-      if port > 0 then
-        yatm_data_logic.emit_value(assigns.pos, port, value)
-      end
+local function check_pad_trigger(pad_id, meta, fields, assigns)
+  local needs_refresh = false
+  local pad_trigger = fields["pad_trigger_" .. pad_id]
+  if pad_trigger then
+    local port = meta:get_int("pad_port_" .. pad_id)
+    local value = meta:get_string("pad_state_value_" .. pad_id .. "_1")
+    if port > 0 then
+      yatm_data_logic.emit_value(assigns.pos, port, value)
     end
   end
-  return true
+
+  local pad_toggle = fields["pad_toggle_" .. pad_id]
+  if pad_toggle then
+    local state = meta:get_int("pad_state_" .. pad_id)
+    if state == 0 then
+      state = 1
+    else
+      state = 0
+    end
+    meta:set_int("pad_state_" .. pad_id, state)
+    needs_refresh = true
+
+    local port = meta:get_int("pad_port_" .. pad_id)
+    local value = meta:get_string("pad_state_value_" .. pad_id .. "_" .. state)
+    if port > 0 then
+      yatm_data_logic.emit_value(assigns.pos, port, value)
+    end
+  end
+
+  return needs_refresh
+end
+
+local function receive_fields(player, form_name, fields, assigns)
+  local meta = minetest.get_meta(assigns.pos)
+  local needs_refresh = false
+
+  for pad_id = 1,assigns.size do
+    if check_pad_trigger(pad_id, meta, fields, assigns) then
+      needs_refresh = true
+    end
+  end
+
+  if needs_refresh then
+    return true, get_formspec(assigns.pos, player, assigns)
+  else
+    return true
+  end
 end
 
 local data_interface = {
@@ -91,12 +160,19 @@ local data_interface = {
   get_programmer_formspec = function (self, pos, user, pointed_thing, assigns)
     --
     local spos = pos.x .. "," .. pos.y .. "," .. pos.z
+    local node = minetest.get_node(pos)
+    local nodedef = minetest.registered_nodes[node.name]
     local meta = minetest.get_meta(pos)
     local inv = meta:get_inventory()
 
     assigns.tab = assigns.tab or 1
-    assigns.width = 4
-    assigns.height = 4
+    assigns.width = nodedef.control_panel.width
+    assigns.height = nodedef.control_panel.height
+    assigns.size = assigns.width * assigns.height
+    assigns.scale = 10 / assigns.width
+
+    local xoffset = (12 - assigns.width * assigns.scale) / 2
+    local yoffset = (12 - assigns.height * assigns.scale) / 2
 
     local formspec =
       "formspec_version[2]" ..
@@ -107,13 +183,13 @@ local data_interface = {
     if assigns.tab == 1 then
       formspec =
         formspec ..
-        render_pads(inv, assigns)
+        render_pads(inv, meta, assigns)
 
     elseif assigns.tab == 2 then
       formspec =
         formspec ..
         "label[0.5,0.75;Pads Setup]" ..
-        "list[nodemeta:" .. spos .. ";pads;4,1;4,4;]" ..
+        "list[nodemeta:" .. spos .. ";pads;0.5,1;" .. assigns.width .. "," .. assigns.height .. ";]" ..
         "list[current_player;main;1,6.85;8,1;]" ..
         "list[current_player;main;1,8.08;8,3;8]" ..
         "listring[nodemeta:" .. spos .. ";pads]" ..
@@ -125,19 +201,19 @@ local data_interface = {
         formspec ..
         "label[0.5,0.75;Ports]"
 
-      for y = 0,3 do
-        local dy = 2 + y * 2
-        for x = 0,3 do
-          local dx = 2.125 + x * 2
+      for y = 0,(assigns.height - 1) do
+        local dy = yoffset + 0.5 + y * assigns.scale
+        for x = 0,(assigns.width - 1) do
+          local dx = xoffset + x * assigns.scale
 
-          local i = 1 + y * 4 + x
+          local i = 1 + y * assigns.width + x
 
           formspec =
             formspec ..
             "field[" .. dx .. "," .. dy ..
-                   ";1.75,1" ..
+                   ";" .. assigns.scale .. ",0.8" ..
                    ";pad_port_" .. i ..
-                   ";Pad Port " .. i ..
+                   ";P" .. i ..
                    ";" .. meta:get_int("pad_port_" .. i) .. "]"
         end
       end
@@ -147,20 +223,46 @@ local data_interface = {
         formspec ..
         "label[0.5,0.75;Data]"
 
-      for y = 0,3 do
-        local dy = 2 + y * 2
-        for x = 0,3 do
-          local dx = 2.125 + x * 2
+      for y = 0,(assigns.height - 1) do
+        local dy = yoffset + 0.5 + y * assigns.scale
+        for x = 0,(assigns.width - 1) do
+          local dx = xoffset + x * assigns.scale
 
-          local i = 1 + y * 4 + x
+          local i = 1 + y * assigns.width + x
 
-          formspec =
-            formspec ..
-            "field[" .. dx .. "," .. dy ..
-                   ";1.75,1" ..
-                   ";pad_value_" .. i ..
-                   ";Pad Value " .. i ..
-                   ";" .. minetest.formspec_escape(meta:get_string("pad_value_" .. i)) .. "]"
+          local stack = inv:get_stack("pads", i)
+
+          if stack:is_empty() then
+            --
+          else
+            local def = stack:get_definition()
+
+            if def.data_control_spec then
+              local spec = def.data_control_spec
+              if spec.type == "momentary_button" then
+                formspec =
+                  formspec ..
+                  "field[" .. dx .. "," .. dy ..
+                         ";2.5,1" ..
+                         ";pad_state_value_" .. i .. "_1" ..
+                         ";T" .. i ..
+                         ";" .. minetest.formspec_escape(meta:get_string("pad_state_value_" .. i .. "_1")) .. "]"
+              elseif spec.type == "switch2" then
+                formspec =
+                  formspec ..
+                  "field[" .. dx .. "," .. dy ..
+                         ";1.125,1" ..
+                         ";pad_state_value_" .. i .. "_0" ..
+                         ";L" .. i ..
+                         ";" .. minetest.formspec_escape(meta:get_string("pad_state_value_" .. i .. "_0")) .. "]" ..
+                  "field[" .. (1.25 + dx) .. "," .. dy ..
+                         ";1.125,1" ..
+                         ";pad_state_value_" .. i .. "_1" ..
+                         ";R" .. i ..
+                         ";" .. minetest.formspec_escape(meta:get_string("pad_state_value_" .. i .. "_1")) .. "]"
+              end
+            end
+          end
         end
       end
     end
@@ -181,24 +283,22 @@ local data_interface = {
       end
     end
 
-    for pad_id = 1,16 do
+    for pad_id = 1,assigns.size do
       local new_pad_port = tonumber(fields["pad_port_" .. pad_id])
       if new_pad_port then
         meta:set_string("pad_port_" .. pad_id, new_pad_port)
       end
 
-      local new_pad_value = fields["pad_value_" .. pad_id]
-      if new_pad_value then
-        meta:set_string("pad_value_" .. pad_id, new_pad_value)
+      for state_id = 0,1 do
+        local field_name = "pad_state_value_" .. pad_id .. "_" .. state_id
+        local new_pad_value = fields[field_name]
+        if new_pad_value then
+          meta:set_string(field_name, new_pad_value)
+        end
       end
 
-      local pad_trigger = fields["pad_trigger_" .. pad_id]
-      if pad_trigger then
-        local port = meta:get_int("pad_port_" .. pad_id)
-        local value = meta:get_string("pad_value_" .. pad_id)
-        if port > 0 then
-          yatm_data_logic.emit_value(assigns.pos, port, value)
-        end
+      if check_pad_trigger(pad_id, meta, fields, assigns) then
+        needs_refresh = true
       end
     end
 
@@ -211,8 +311,8 @@ local data_interface = {
   end,
 }
 
-minetest.register_node("yatm_data_control:data_control_plane", {
-  description = "Data Control Plane [4x4]",
+yatm.register_stateful_node("yatm_data_control:data_control_plane", {
+  base_description = "Data Control Plane",
 
   codex_entry_id = "yatm_data_control:data_control_plane",
 
@@ -246,7 +346,9 @@ minetest.register_node("yatm_data_control:data_control_plane", {
   on_construct = function (pos)
     local meta = minetest.get_meta(pos)
     local inv = meta:get_inventory()
-    inv:set_size("pads", 16)
+    local node = minetest.get_node(pos)
+    local nodedef = minetest.registered_nodes[node.name]
+    inv:set_size("pads", nodedef.control_panel.width * nodedef.control_panel.height)
     local node = minetest.get_node(pos)
     data_network:add_node(pos, node)
   end,
@@ -263,6 +365,7 @@ minetest.register_node("yatm_data_control:data_control_plane", {
   refresh_infotext = function (pos)
     local meta = minetest.get_meta(pos)
     local infotext =
+      "Control Panel\n" ..
       data_network:get_infotext(pos)
 
     meta:set_string("infotext", infotext)
@@ -283,4 +386,31 @@ minetest.register_node("yatm_data_control:data_control_plane", {
       formspec
     )
   end,
+}, {
+  ["2x2"] = {
+    description = "Data Control Plane [2x2]",
+
+    control_panel = {
+      width = 2,
+      height = 2,
+    },
+  },
+
+  ["4x4"] = {
+    description = "Data Control Plane [4x4]",
+
+    control_panel = {
+      width = 4,
+      height = 4,
+    },
+  },
+
+  ["8x8"] = {
+    description = "Data Control Plane [8x8]",
+
+    control_panel = {
+      width = 8,
+      height = 8,
+    },
+  },
 })
