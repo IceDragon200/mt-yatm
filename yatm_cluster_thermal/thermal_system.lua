b96856146ae0c2253f9b35162d31fa707359b882
Corey Powell <mistdragon100@gmail.com>
2020-01-26 20:49:48 -0500

    yatm_cluster_thermal: Fixed issue in thermal cluster where node is nil

--- yatm_cluster_thermal/thermal_system.lua
+++ yatm_cluster_thermal/thermal_system.lua
@@ -16,28 +16,30 @@ function ic:update(cls, cluster, dtime)
 
   cluster:reduce_nodes_of_groups("thermal_producer", 0, function (node_entry, acc)
     local node = minetest.get_node_or_nil(node_entry.pos)
-    local nodedef = minetest.registered_nodes[node.name]
+    if node then
+      local nodedef = minetest.registered_nodes[node.name]
 
-    if not nodedef.thermal_interface then
-      error("expected a thermal_interface node=" .. node.name)
-    end
+      if not nodedef.thermal_interface then
+        error("expected a thermal_interface node=" .. node.name)
+      end
 
-    local heat_produced = nodedef.thermal_interface:get_heat(node_entry.pos, node, dtime)
+      local heat_produced = nodedef.thermal_interface:get_heat(node_entry.pos, node, dtime)
 
-    --print("Updating Cluster", cluster.id, dtime, heat_produced)
-    for dir, vec3 in pairs(yatm_core.DIR6_TO_VEC3) do
-      local neighbour_pos = vector.add(node_entry.pos, vec3)
-      local neighbour_node_entry = cluster:get_node(neighbour_pos)
-      if neighbour_node_entry then
-        local neighbour_node = minetest.get_node(neighbour_node_entry.pos)
-        local neighbour_nodedef = minetest.registered_nodes[neighbour_node.name]
+      --print("Updating Cluster", cluster.id, dtime, heat_produced)
+      for dir, vec3 in pairs(yatm_core.DIR6_TO_VEC3) do
+        local neighbour_pos = vector.add(node_entry.pos, vec3)
+        local neighbour_node_entry = cluster:get_node(neighbour_pos)
+        if neighbour_node_entry then
+          local neighbour_node = minetest.get_node(neighbour_node_entry.pos)
+          local neighbour_nodedef = minetest.registered_nodes[neighbour_node.name]
 
-        if not neighbour_nodedef.thermal_interface then
-          error("expected a thermal_interface node=" .. neighbour_node.name)
-        end
+          if not neighbour_nodedef.thermal_interface then
+            error("expected a thermal_interface node=" .. neighbour_node.name)
+          end
 
-        if neighbour_nodedef.thermal_interface.update_heat then
-          neighbour_nodedef.thermal_interface:update_heat(neighbour_pos, neighbour_node, heat_produced, dtime)
+          if neighbour_nodedef.thermal_interface.update_heat then
+            neighbour_nodedef.thermal_interface:update_heat(neighbour_pos, neighbour_node, heat_produced, dtime)
+          end
         end
       end
     end
