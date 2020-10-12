c50cc217c46e72aeeec48199c4dd96f4268f6bd8
Corey Powell <mistdragon100@gmail.com>
2020-10-12 18:34:42 -0500

    yatm_overhead_rails: Added missing version string in new_module call

--- yatm_overhead_rails/init.lua
+++ yatm_overhead_rails/init.lua
@@ -3,7 +3,7 @@
   Overhead Rails
 
 ]]
-local mod = foundation.new_module("yatm_overhead_rails")
+local mod = foundation.new_module("yatm_overhead_rails", "0.0.1")
 
 mod:require("nodes.lua")
 mod:require("items.lua")
