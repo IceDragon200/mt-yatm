c9fa2b273df4984f1b4054e5a27f9c136b59109d
Corey Powell <mistdragon100@gmail.com>
2020-10-15 01:14:31 -0500

    yatm_codex_entries: New sawmill entry and updated deps
    Some modules are still missing however

---
+++ yatm_codex_entries/entries/yatm_woodcraft.lua
@@ -0,0 +1,14 @@
+yatm.codex.register_entry("yatm_woodcraft:sawmill", {
+  pages = {
+    {
+      heading_item = {
+        context = true,
+        default = "yatm_woodcraft:sawmill",
+      },
+      heading = "Sawmill",
+      lines = {
+        "A simple machine for cutting wood into more components.",
+      },
+    },
+  },
+})
