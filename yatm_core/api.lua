e74fd236d5a43a4bf2365d15729b06d6cced763e
Corey Powell <mistdragon100@gmail.com>
2020-10-15 01:14:09 -0500

    yatm_core: Added player_inventory_lists_fragment/3

--- yatm_core/api.lua
+++ yatm_core/api.lua
@@ -4,6 +4,7 @@
 
 ]]
 local table_merge = assert(foundation.com.table_merge)
+local fspec = assert(foundation.com.formspec.api)
 
 -- alias foundation modules into yatm, they were originally yatm modules to begin with
 yatm.Luna = foundation.com.Luna
@@ -18,6 +19,8 @@ yatm.Vector4 = foundation.com.Vector4
 yatm.Cuboid = foundation.com.Cuboid
 
 if rawget(_G, "nokore_player_inv") then
+  yatm.player_inventory_lists_fragment = nokore_player_inv.player_inventory_lists_fragment
+
   function yatm.get_player_hotbar_size(_player)
     return nokore_player_inv.player_hotbar_size
   end
@@ -25,6 +28,30 @@ else
   function yatm.get_player_hotbar_size(_player)
     -- minetest game's default size
     return 8
+  end
+
+  function yatm.player_inventory_lists_fragment(player, x, y)
+    local size = yatm.get_player_hotbar_size(player)
+    local inv = player:get_inventory()
+
+    local h = 0
+
+    local main_size = inv:get_size("main")
+    local offhand_size = main_size - size
+
+    local result = ""
+
+    result = result .. fspec.list("current_player", "main", x, y, size, 1)
+
+    h = 1.25
+
+    if offhand_size > 0 then
+      local rows = math.ceil(offhand_size / size)
+      result = result .. fspec.list("current_player", "main", x, h + y, size, rows, size)
+      h = h + rows
+    end
+
+    return result, h
   end
 end
 
