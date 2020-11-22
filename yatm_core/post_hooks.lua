ecb43cfdf0b73e0b02479b0a71f4e2d3e292c90b
Corey Powell <mistdragon100@gmail.com>
2020-11-21 22:51:05 -0500

    yatm_core: Dump all items/nodes

--- yatm_core/post_hooks.lua
+++ yatm_core/post_hooks.lua
@@ -9,24 +9,21 @@ local function dump_nodes()
   local i = 0
   local result = {}
   for name, def in pairs(minetest.registered_nodes) do
-    if string_starts_with(name, "yatm_") or
-       string_starts_with(name, "harmonia_") then
-      i = i + 1
+    i = i + 1
 
-      result[i] = minetest.write_json({
-        name = name,
-        basename = def.basename or name,
-        base_description = def.base_description,
-        description = def.description,
-        groups = def.groups,
-        tiles = def.tiles,
-        special_tiles = def.special_tiles,
-        drawtype = def.drawtype,
-        node_box = def.node_box,
-        paramtype = def.paramtype,
-        paramtype2 = def.paramtype2,
-      })
-    end
+    result[i] = minetest.write_json({
+      name = name,
+      basename = def.basename or name,
+      base_description = def.base_description,
+      description = def.description,
+      groups = def.groups,
+      tiles = def.tiles,
+      special_tiles = def.special_tiles,
+      drawtype = def.drawtype,
+      node_box = def.node_box,
+      paramtype = def.paramtype,
+      paramtype2 = def.paramtype2,
+    })
   end
 
   minetest.safe_file_write(minetest.get_worldpath() .. "/yatm_exported_nodes.mljson", table.concat(result, "\n"))
@@ -40,18 +37,15 @@ local function dump_craftitems()
   local result = {}
 
   for name, def in pairs(minetest.registered_craftitems) do
-    if string_starts_with(name, "yatm_") or
-       string_starts_with(name, "harmonia_") then
-      i = i + 1
-      result[i] = minetest.write_json({
-        name = name,
-        basename = def.basename or name,
-        base_description = def.base_description,
-        description = def.description,
-        groups = def.groups,
-        inventory_image = def.inventory_image,
-      })
-    end
+    i = i + 1
+    result[i] = minetest.write_json({
+      name = name,
+      basename = def.basename or name,
+      base_description = def.base_description,
+      description = def.description,
+      groups = def.groups,
+      inventory_image = def.inventory_image,
+    })
   end
 
   minetest.safe_file_write(minetest.get_worldpath() .. "/yatm_exported_craftitems.mljson", table.concat(result, "\n"))
@@ -65,18 +59,15 @@ local function dump_tools()
   local result = {}
 
   for name, def in pairs(minetest.registered_tools) do
-    if string_starts_with(name, "yatm_") or
-       string_starts_with(name, "harmonia_") then
-      i = i + 1
-      result[i] = minetest.write_json({
-        name = name,
-        basename = def.basename or name,
-        base_description = def.base_description,
-        description = def.description,
-        groups = def.groups,
-        inventory_image = def.inventory_image,
-      })
-    end
+    i = i + 1
+    result[i] = minetest.write_json({
+      name = name,
+      basename = def.basename or name,
+      base_description = def.base_description,
+      description = def.description,
+      groups = def.groups,
+      inventory_image = def.inventory_image,
+    })
   end
 
   minetest.safe_file_write(minetest.get_worldpath() .. "/yatm_exported_tools.mljson", table.concat(result, "\n"))
