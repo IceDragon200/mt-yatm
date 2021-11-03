35ffdccf0d8382e3e17e49242292e62da101524b
Corey Powell <mistdragon100@gmail.com>
2021-11-03 00:34:30 -0500

    yatm_{device_hubs,machines}: Added card hub and moved hub textures

--- yatm_device_hubs/nodes.lua
+++ yatm_device_hubs/nodes.lua
@@ -1,3 +1,4 @@
 yatm_device_hubs:require("nodes/bus_hub.lua")
+yatm_device_hubs:require("nodes/card_hub.lua")
 yatm_device_hubs:require("nodes/element_hub.lua")
 yatm_device_hubs:require("nodes/wireless_hub.lua")
---
+++ yatm_device_hubs/nodes/card_hub.lua
@@ -0,0 +1,77 @@
+local yatm_network = {
+  kind = "hub",
+  groups = {
+    hub = 1,
+    energy_consumer = 1,
+  },
+  default_state = "off",
+  states = {
+    error = "yatm_device_hubs:hub_card_error",
+    conflict = "yatm_device_hubs:hub_card_error",
+    off = "yatm_device_hubs:hub_card_off",
+    on = "yatm_device_hubs:hub_card_on",
+  },
+  energy = {
+    capacity = 200,
+    passive_lost = 0,
+    network_charge_bandwidth = 10,
+    startup_threshold = 1,
+  }
+}
+
+yatm.devices.register_stateful_network_device({
+  basename = "yatm_device_hubs:hub_card",
+
+  description = "Hub (Card)",
+
+  groups = {cracky = 1},
+
+  drop = yatm_network.states.off,
+
+  use_texture_alpha = "clip",
+  tiles = {
+    "yatm_card_hub_top.png",
+    "yatm_card_hub_bottom.png",
+    "yatm_card_hub_side.png",
+    "yatm_card_hub_side.png^[transformFX",
+    "yatm_card_hub_side.png",
+    "yatm_card_hub_front.png",
+  },
+  drawtype = "nodebox",
+  node_box = {
+    type = "fixed",
+    fixed = {
+      {-0.375, -0.5, -0.375, 0.375, (5 / 16.0) - 0.5, 0.375},
+    }
+  },
+
+  paramtype = "light",
+  paramtype2 = "facedir",
+
+  after_place_node = yatm_device_hubs.hub_after_place_node,
+
+  yatm_network = yatm_network,
+
+  refresh_infotext = yatm_device_hubs.hub_refresh_infotext,
+}, {
+  error = {
+    tiles = {
+      "yatm_card_hub_top.png",
+      "yatm_card_hub_bottom.png",
+      "yatm_card_hub_side.png",
+      "yatm_card_hub_side.png^[transformFX",
+      "yatm_card_hub_side.png",
+      "yatm_card_hub_front.png",
+    },
+  },
+  on = {
+    tiles = {
+      "yatm_card_hub_top.png",
+      "yatm_card_hub_bottom.png",
+      "yatm_card_hub_side.png",
+      "yatm_card_hub_side.png^[transformFX",
+      "yatm_card_hub_side.png",
+      "yatm_card_hub_front.png",
+    },
+  },
+})
