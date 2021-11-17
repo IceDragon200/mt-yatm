8e361a236a5552ce6d3a1a873e8fdec23fd3ac95
Corey Powell <mistdragon100@gmail.com>
2021-11-16 20:41:02 -0500

    yatm_papercraft: Added some fluid box code

--- yatm_papercraft/items/fluid_box.lua
+++ yatm_papercraft/items/fluid_box.lua
@@ -1,11 +1,23 @@
---[[
+--
+-- Fluid Box are small fluid carrying items, they are made from waxed cardboard.
+--
+-- Please don't carry lava in them.
+--
+local function pickup_fluid_box(item_stack, user, pointed_thing)
+  -- FIXME: pickup fluid
+  return item_stack
+end
 
-  Fluid Box are small fluid carrying items, they are made from waxed cardboard.
+local function place_fluid_box_contents(item_stack, user, pointed_thing)
+  -- FIXME: drop fluid
+  return item_stack
+end
 
-  Please don't carry lava in them.
-
-]]
-minetest.register_craftitem("yatm_papercraft:fluid_box", {
+minetest.register_tool("yatm_papercraft:fluid_box", {
   description = "Fluid Box",
+
   inventory_image = "yatm_fluid_box.png",
+
+  on_use = pickup_fluid_box,
+  on_place = place_fluid_box_contents,
 })
