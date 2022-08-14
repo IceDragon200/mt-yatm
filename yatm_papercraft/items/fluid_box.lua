1d509642198267aa2727c9b7d1e2e3376bf2d0e0
Corey Powell <mistdragon100@gmail.com>
2022-08-14 15:34:52 -0500

    yatm_papercraft: Make Fluid Boxes fluid containers

--- yatm_papercraft/items/fluid_box.lua
+++ yatm_papercraft/items/fluid_box.lua
@@ -3,6 +3,8 @@
 --
 -- Please don't carry lava in them.
 --
+local mod = assert(yatm_papercraft)
+
 local function pickup_fluid_box(item_stack, user, pointed_thing)
   -- FIXME: pickup fluid
   return item_stack
@@ -14,10 +16,19 @@ local function place_fluid_box_contents(item_stack, user, pointed_thing)
 end
 
 minetest.register_tool("yatm_papercraft:fluid_box", {
-  description = "Fluid Box",
+  description = mod.S("Fluid Box"),
+
+  groups = {
+    fluid_container = 1,
+  },
 
   inventory_image = "yatm_fluid_box.png",
 
   on_use = pickup_fluid_box,
   on_place = place_fluid_box_contents,
+
+  fluid_container = {
+    key = "fluid",
+    capacity = 1000,
+  },
 })
