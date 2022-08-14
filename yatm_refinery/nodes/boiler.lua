d90ed945d0baaeec228d376e58b0e28bcd0bd46e
Corey Powell <mistdragon100@gmail.com>
2022-08-14 15:36:55 -0500

    yatm_refinery: Formspec for electric boiler

--- yatm_refinery/nodes/boiler.lua
+++ yatm_refinery/nodes/boiler.lua
@@ -1,3 +1,6 @@
+local mod = assert(yatm_refinery)
+local fspec = assert(foundation.com.formspec.api)
+local fluid_fspec = assert(yatm.fluids.formspec)
 local Groups = assert(foundation.com.Groups)
 local Directions = assert(foundation.com.Directions)
 local cluster_devices = assert(yatm.cluster.devices)
@@ -8,6 +11,8 @@ local FluidTanks = assert(yatm.fluids.FluidTanks)
 local FluidUtils = assert(yatm.fluids.Utils)
 local FluidMeta = assert(yatm.fluids.FluidMeta)
 local Energy = assert(yatm.energy)
+local Vector3 = assert(foundation.com.Vector3)
+local player_service = assert(nokore.player_service)
 
 local boiler_yatm_network = {
   kind = "machine",
@@ -33,6 +38,7 @@ local boiler_yatm_network = {
 
 local STEAM_TANK = "steam_tank"
 local WATER_TANK = "water_tank"
+local TANK_CAPACITY = 16000
 
 local function get_fluid_tank_name(self, pos, dir)
   local node = minetest.get_node(pos)
@@ -46,7 +52,7 @@ local function get_fluid_tank_name(self, pos, dir)
 end
 
 local fluid_interface = FluidInterface.new_directional(get_fluid_tank_name)
-fluid_interface._private.capacity = 16000
+fluid_interface._private.capacity = TANK_CAPACITY
 fluid_interface._private.bandwidth = fluid_interface._private.capacity
 
 function fluid_interface:on_fluid_changed(pos, dir, _new_stack)
@@ -113,23 +119,38 @@ function boiler_yatm_network:work(ctx)
 
   -- Convert water into steam
   do
-    local stack = FluidMeta.drain_fluid(meta,
-      WATER_TANK,
-      FluidStack.new("group:water", math.floor(500 * dtime)),
-      fluid_interface._private.bandwidth, fluid_interface._private.capacity, false)
+    local stack =
+      FluidMeta.drain_fluid(
+        meta,
+        WATER_TANK,
+        FluidStack.new("group:water", math.floor(1000 * dtime)),
+        fluid_interface._private.bandwidth,
+        fluid_interface._private.capacity,
+        false
+      )
 
     if stack then
       -- TODO: yatm_core:steam should not be hardcoded
-      local filled_stack = FluidMeta.fill_fluid(meta,
-        STEAM_TANK,
-        FluidStack.set_name(stack, "yatm_core:steam"),
-        fluid_interface._private.bandwidth, fluid_interface._private.capacity, true)
+      local filled_stack =
+        FluidMeta.fill_fluid(
+          meta,
+          STEAM_TANK,
+          FluidStack.set_name(stack, "yatm_core:steam"),
+          fluid_interface._private.bandwidth,
+          fluid_interface._private.capacity,
+          true
+        )
 
       if filled_stack and filled_stack.amount > 0 then
-        FluidMeta.drain_fluid(meta,
+        FluidMeta.drain_fluid(
+          meta,
           WATER_TANK,
           FluidStack.set_amount(stack, filled_stack.amount),
-          fluid_interface._private.bandwidth, fluid_interface._private.capacity, true)
+          fluid_interface._private.bandwidth,
+          fluid_interface._private.capacity,
+          true
+        )
+
         energy_consumed = energy_consumed + filled_stack.amount
       end
     end
@@ -137,10 +158,15 @@ function boiler_yatm_network:work(ctx)
 
   -- Fill tank on the UP face of the boiler with steam, if available
   do
-    local stack, _new_stack = FluidMeta.drain_fluid(meta,
-      STEAM_TANK,
-      FluidStack.new("group:steam", math.floor(1000 * dtime)),
-      fluid_interface._private.capacity, fluid_interface._private.capacity, false)
+    local stack, _new_stack =
+      FluidMeta.drain_fluid(
+        meta,
+        STEAM_TANK,
+        FluidStack.new("group:steam", math.floor(1000 * dtime)),
+        fluid_interface._private.capacity,
+        fluid_interface._private.capacity,
+        false
+      )
 
     if stack then
       local steam_tank_dir = Directions.facedir_to_face(node.param2, Directions.D_UP)
@@ -149,8 +175,14 @@ function boiler_yatm_network:work(ctx)
       local steam_tank_nodedef = minetest.registered_nodes[steam_tank_node.name]
 
       if steam_tank_nodedef then
-        local filled_stack = FluidTanks.fill_fluid(steam_tank_pos,
-          Directions.invert_dir(steam_tank_dir), stack, true)
+        local filled_stack =
+          FluidTanks.fill_fluid(
+            steam_tank_pos,
+            Directions.invert_dir(steam_tank_dir),
+            stack,
+            true
+          )
+
         if filled_stack and filled_stack.amount > 0 then
           FluidTanks.drain_fluid(pos, steam_tank_dir, filled_stack, true)
           energy_consumed = energy_consumed + 1
@@ -162,10 +194,72 @@ function boiler_yatm_network:work(ctx)
   return energy_consumed
 end
 
+local function render_formspec(pos, user, state)
+  local spos = pos.x .. "," .. pos.y .. "," .. pos.z
+  local node_inv_name = "nodemeta:" .. spos
+  local cio = fspec.calc_inventory_offset
+  local meta = minetest.get_meta(pos)
+
+  return yatm.formspec_render_split_inv_panel(user, 8, 4, { bg = "machine" }, function (loc, rect)
+    if loc == "main_body" then
+      local steam_stack = FluidMeta.get_fluid_stack(meta, STEAM_TANK)
+      local water_stack = FluidMeta.get_fluid_stack(meta, WATER_TANK)
+
+      return fluid_fspec.render_fluid_stack(rect.x, rect.y, 1, cio(4), steam_stack, TANK_CAPACITY) ..
+        fluid_fspec.render_fluid_stack(rect.x + cio(7), rect.y, 1, cio(4), water_stack, TANK_CAPACITY)
+    elseif loc == "footer" then
+      return ""
+    end
+    return ""
+  end)
+end
+
+local function on_receive_fields(player, form_name, fields, state)
+  return false, nil
+end
+
+local function make_formspec_name(pos)
+  return "yatm_refinery:boiler:"..Vector3.to_string(pos)
+end
+
+local function on_refresh_timer(player_name, form_name, state)
+  local player = player_service:get_player_by_name(player_name)
+  return {
+    {
+      type = "refresh_formspec",
+      value = render_formspec(state.pos, player, state),
+    }
+  }
+end
+
+local function on_rightclick(pos, node, user)
+  local state = {
+    pos = pos,
+  }
+  local formspec = render_formspec(pos, user, state)
+
+  nokore.formspec_bindings:show_formspec(
+    user:get_player_name(),
+    make_formspec_name(pos),
+    formspec,
+    {
+      state = state,
+      on_receive_fields = on_receive_fields,
+      timers = {
+        -- steam turbines have a fluid tank, so their formspecs need to be routinely updated
+        refresh = {
+          every = 1,
+          action = on_refresh_timer,
+        },
+      },
+    }
+  )
+end
+
 yatm.devices.register_stateful_network_device({
   basename = "yatm_refinery:boiler",
 
-  description = "Boiler",
+  description = mod.S("Boiler"),
 
   codex_entry_id = "yatm_refinery:boiler",
 
@@ -195,6 +289,8 @@ yatm.devices.register_stateful_network_device({
   fluid_interface = fluid_interface,
 
   refresh_infotext = boiler_refresh_infotext,
+
+  on_rightclick = on_rightclick,
 }, {
   error = {
     tiles = {
