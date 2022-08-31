3091757feea38965310f5b11a93823ca3ad9e39b
Corey Powell <mistdragon100@gmail.com>
2022-08-30 22:42:56 -0500

    yatm_mining: Quarry energy gauge and formspec

--- yatm_mining/nodes/quarry.lua
+++ yatm_mining/nodes/quarry.lua
@@ -1,11 +1,14 @@
 local Directions = assert(foundation.com.Directions)
+local Energy = assert(yatm.energy)
 local ItemInterface = assert(yatm.items.ItemInterface)
+local Vector3 = assert(foundation.com.Vector3)
+local fspec = assert(foundation.com.formspec.api)
+local yatm_fspec = assert(yatm.formspec)
+local player_service = assert(nokore.player_service)
 
 local quarry_item_interface = ItemInterface.new_simple("main")
 
-local function quarry_on_construct(pos)
-  local meta = minetest.get_meta(pos)
-
+local function maybe_initialize_inventory(meta)
   local inv = meta:get_inventory()
 
   inv:set_size("main", 4) -- Quarry has a small internal inventory
@@ -16,11 +19,17 @@ local function quarry_on_construct(pos)
 
   meta:set_int("dx", 1)
   meta:set_int("dz", 1)
+end
+
+local function on_construct(pos)
+  local meta = minetest.get_meta(pos)
+
+  maybe_initialize_inventory(meta)
 
   yatm.devices.device_on_construct(pos)
 end
 
-local quarry_yatm_network = {
+local yatm_network = {
   kind = "machine",
 
   groups = {
@@ -45,7 +54,7 @@ local quarry_yatm_network = {
   }
 }
 
-function quarry_yatm_network:work(ctx)
+function yatm_network:work(ctx)
   local pos = ctx.pos
   local meta = ctx.meta
   local node = ctx.node
@@ -129,6 +138,74 @@ function quarry_yatm_network:work(ctx)
   return 0
 end
 
+local function render_formspec(pos, user, state)
+  local spos = pos.x .. "," .. pos.y .. "," .. pos.z
+  local node_inv_name = "nodemeta:" .. spos
+  local cio = fspec.calc_inventory_offset
+  local cis = fspec.calc_inventory_size
+  local meta = minetest.get_meta(pos)
+
+  return yatm.formspec_render_split_inv_panel(user, nil, 4, { bg = "machine" }, function (loc, rect)
+    if loc == "main_body" then
+      return yatm_fspec.render_meta_energy_gauge(
+          rect.x + rect.w - cio(1),
+          rect.y,
+          1,
+          rect.h,
+          meta,
+          yatm.devices.ENERGY_BUFFER_KEY,
+          yatm.devices.get_energy_capacity(pos, state.node)
+        )
+    elseif loc == "footer" then
+      return ""
+    end
+    return ""
+  end)
+end
+
+local function on_receive_fields(player, form_name, fields, state)
+  return false, nil
+end
+
+local function make_formspec_name(pos)
+  return "yatm_machines:quarry:"..Vector3.to_string(pos)
+end
+
+local function on_refresh_timer(player_name, form_name, state)
+  local player = player_service:get_player_by_name(player_name)
+  return {
+    {
+      type = "refresh_formspec",
+      value = render_formspec(state.pos, player, state),
+    }
+  }
+end
+
+local function on_rightclick(pos, node, user)
+  local state = {
+    pos = pos,
+    node = node,
+  }
+  local formspec = render_formspec(pos, user, state)
+
+  nokore.formspec_bindings:show_formspec(
+    user:get_player_name(),
+    make_formspec_name(pos),
+    formspec,
+    {
+      state = state,
+      on_receive_fields = on_receive_fields,
+      timers = {
+        -- routinely update the formspec
+        refresh = {
+          every = 1,
+          action = on_refresh_timer,
+        },
+      },
+    }
+  )
+end
+
 yatm.devices.register_stateful_network_device({
   basename = "yatm_mining:quarry",
 
@@ -136,7 +213,7 @@ yatm.devices.register_stateful_network_device({
 
   codex_entry_id = "yatm_mining:quarry",
 
-  drop = quarry_yatm_network.states.off,
+  drop = yatm_network.states.off,
 
   groups = {
     cracky = 1,
@@ -155,11 +232,13 @@ yatm.devices.register_stateful_network_device({
   paramtype = "none",
   paramtype2 = "facedir",
 
-  on_construct = quarry_on_construct,
+  on_construct = on_construct,
 
   item_interface = quarry_item_interface,
 
-  yatm_network = quarry_yatm_network,
+  yatm_network = yatm_network,
+
+  on_rightclick = on_rightclick,
 }, {
   error = {
     tiles = {
