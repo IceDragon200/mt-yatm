cc8e74644bd112ab8bcbf9d8e78d943bbe98e601
Corey Powell <mistdragon100@gmail.com>
2022-09-04 21:50:59 -0500

    yatm_brewery: Load basin and mechanical press

--- yatm_brewery/nodes.lua
+++ yatm_brewery/nodes.lua
@@ -1,3 +1,5 @@
 yatm_brewery:require("nodes/fluid_barrel.lua")
 yatm_brewery:require("nodes/aging_barrel.lua")
 yatm_brewery:require("nodes/kettle.lua")
+yatm_brewery:require("nodes/basin.lua")
+yatm_brewery:require("nodes/mechanical_press.lua")
