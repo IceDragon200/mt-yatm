6b770eb1e70d6949ecd09cc603ef8aa5f7f1810b
Corey Powell <mistdragon100@gmail.com>
2022-09-04 21:52:18 -0500

    yatm_mining: SurfaceDrill formspec

--- yatm_mining/nodes/surface_drill.lua
+++ yatm_mining/nodes/surface_drill.lua
@@ -1,4 +1,24 @@
 local Directions = assert(foundation.com.Directions)
+local Energy = assert(yatm.energy)
+local ItemInterface = assert(yatm.items.ItemInterface)
+local Vector3 = assert(foundation.com.Vector3)
+local fspec = assert(foundation.com.formspec.api)
+local yatm_fspec = assert(yatm.formspec)
+local player_service = assert(nokore.player_service)
+
+local function maybe_initialize_inventory(meta)
+  local inv = meta:get_inventory()
+
+  inv:set_size("main", 4) -- Surface drill's have a small internal inventory
+end
+
+local function on_construct(pos)
+  local meta = minetest.get_meta(pos)
+
+  maybe_initialize_inventory(meta)
+
+  yatm.devices.device_on_construct(pos)
+end
 
 local surface_drill_yatm_network = {
   kind = "machine",
@@ -104,6 +124,74 @@ function surface_drill_yatm_network:work(ctx)
   return 0
 end
 
+local function render_formspec(pos, user, state)
+  local spos = pos.x .. "," .. pos.y .. "," .. pos.z
+  local node_inv_name = "nodemeta:" .. spos
+  local cio = fspec.calc_inventory_offset
+  local cis = fspec.calc_inventory_size
+  local meta = minetest.get_meta(pos)
+
+  return yatm.formspec_render_split_inv_panel(user, nil, 4, { bg = "machine" }, function (loc, rect)
+    if loc == "main_body" then
+      return yatm_fspec.render_meta_energy_gauge(
+          rect.x + rect.w - cio(1),
+          rect.y,
+          1,
+          rect.h,
+          meta,
+          yatm.devices.ENERGY_BUFFER_KEY,
+          yatm.devices.get_energy_capacity(pos, state.node)
+        )
+    elseif loc == "footer" then
+      return ""
+    end
+    return ""
+  end)
+end
+
+local function on_receive_fields(player, form_name, fields, state)
+  return false, nil
+end
+
+local function make_formspec_name(pos)
+  return "yatm_machines:surface_drill:"..Vector3.to_string(pos)
+end
+
+local function on_refresh_timer(player_name, form_name, state)
+  local player = player_service:get_player_by_name(player_name)
+  return {
+    {
+      type = "refresh_formspec",
+      value = render_formspec(state.pos, player, state),
+    }
+  }
+end
+
+local function on_rightclick(pos, node, user)
+  local state = {
+    pos = pos,
+    node = node,
+  }
+  local formspec = render_formspec(pos, user, state)
+
+  nokore.formspec_bindings:show_formspec(
+    user:get_player_name(),
+    make_formspec_name(pos),
+    formspec,
+    {
+      state = state,
+      on_receive_fields = on_receive_fields,
+      timers = {
+        -- routinely update the formspec
+        refresh = {
+          every = 1,
+          action = on_refresh_timer,
+        },
+      },
+    }
+  )
+end
+
 yatm.devices.register_stateful_network_device({
   basename = "yatm_mining:surface_drill",
 
@@ -111,7 +199,11 @@ yatm.devices.register_stateful_network_device({
 
   codex_entry_id = "yatm_mining:surface_drill",
 
-  groups = {cracky = 1, surface_drill = 1},
+  groups = {
+    cracky = nokore.dig_class("iron"),
+    --
+    surface_drill = 1
+  },
 
   drop = surface_drill_yatm_network.states.off,
 
@@ -128,6 +220,8 @@ yatm.devices.register_stateful_network_device({
   paramtype = "none",
   paramtype2 = "facedir",
   yatm_network = surface_drill_yatm_network,
+
+  on_rightclick = on_rightclick,
 }, {
   error = {
     tiles = {
@@ -178,7 +272,7 @@ yatm.devices.register_stateful_network_device({
   codex_entry_id = "yatm_mining:surface_drill_ext",
 
   groups = {
-    cracky = 1,
+    cracky = nokore.dig_class("copper"),
     surface_drill_ext = 1
   },
 
@@ -226,7 +320,8 @@ minetest.register_node("yatm_mining:surface_drill_bit", {
   codex_entry_id = "yatm_mining:surface_drill_bit",
 
   groups = {
-    cracky = 1,
+    cracky = nokore.dig_class("copper"),
+    --
     surface_drill_bit = 1,
     not_in_creative_inventory = 1
   },
