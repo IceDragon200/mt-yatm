ab4e00dc6b2f35150df9d6b2ccebbceeb0bd76c1
Corey Powell <mistdragon100@gmail.com>
2023-03-31 18:27:54 -0500

    yatm_core: Warn that decor blocks may be unavailable when nokore_stairs is unavailable

--- yatm_core/api/building_blocks.lua
+++ yatm_core/api/building_blocks.lua
@@ -7,8 +7,11 @@ local nokore_stairs = rawget(_G, "nokore_stairs")
 
 -- @spec build_decor_nodes(Table): Table
 if nokore_stairs then
+  minetest.log("info", "using nokore_stairs.build_nodes/2 for decor nodes")
   yatm.build_decor_nodes = nokore_stairs.build_nodes
 else
+  minetest.log("warning", "using yatm.build_decor_nodes/2 for decor nodes, this is a no-op function")
+
   function yatm.build_decor_nodes(_data)
     return {}
   end
