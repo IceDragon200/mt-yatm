43b7fb8f9f03e1ad52667dfe9c3d63dcb0cf6024
Corey Powell <mistdragon100@gmail.com>
2023-04-03 21:41:16 -0500

    yatm_mininig: Fix bug where nil node would crash quarry or mining drills

--- yatm_mining/api.lua
+++ yatm_mining/api.lua
@@ -1,36 +1,38 @@
 local add_items = assert(foundation.com.InventoryList.add_items)
 local item_list_copy = assert(foundation.com.InventoryList.copy)
 
--- @namespace yatm.mining
+--- @namespace yatm.mining
 
 yatm.mining = yatm.mining or {}
 
---
--- Specialized dig/remove node function for quarries and surface drills
---
--- @spec drill_node_to_meta_inventory(
---   pos: Vector3,
---   meta: MetaRef,
---   list_name: String
--- ): NodeRef
+---
+--- Specialized dig/remove node function for quarries and surface drills
+---
+--- @spec drill_node_to_meta_inventory(
+---   pos: Vector3,
+---   meta: MetaRef,
+---   list_name: String
+--- ): NodeRef
 function yatm.mining.drill_node_to_meta_inventory(pos, meta, list_name)
   local node = minetest.get_node_or_nil(pos)
-  if node.name == "air" then
-    return node
-  elseif node then
-    local drops = minetest.get_node_drops(node, nil)
+  if node then
+    if node.name == "air" then
+      return node
+    else
+      local drops = minetest.get_node_drops(node, nil)
 
-    local inv = meta:get_inventory()
-    local list = inv:get_list(list_name)
-    if list then
-      list = item_list_copy(list)
+      local inv = meta:get_inventory()
+      local list = inv:get_list(list_name)
+      if list then
+        list = item_list_copy(list)
 
-      local leftovers = add_items(list, drops)
+        local leftovers = add_items(list, drops)
 
-      if not next(leftovers) then
-        inv:set_list(list_name, list)
-        minetest.remove_node(pos)
-        return node
+        if not next(leftovers) then
+          inv:set_list(list_name, list)
+          minetest.remove_node(pos)
+          return node
+        end
       end
     end
   end
