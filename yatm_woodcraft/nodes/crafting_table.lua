6aeca60d2a302ee6201d3db7c8ed69276cf0db96
Corey Powell <mistdragon100@gmail.com>
2023-09-09 13:29:51 -0500

    yatm_woodcraft: Touch up crafting table

--- yatm_woodcraft/nodes/crafting_table.lua
+++ yatm_woodcraft/nodes/crafting_table.lua
@@ -33,19 +33,19 @@ local function get_crafting_table_formspec(pos, user)
 
   local cio = fspec.calc_inventory_offset
 
-  return yatm.formspec_render_split_inv_panel(user, nil, 4, { bg = "wood" }, function (loc, rect)
+  return yatm.formspec_render_split_inv_panel(user, 8, 4, { bg = "wood" }, function (loc, rect)
     if loc == "main_body" then
-      return "" ..
-        fspec.list(my_inv_name, "main", rect.x, rect.y + cio(0), 2, 4) ..
-        fspec.list(my_inv_name, "crafting_grid", rect.x + cio(2.5), rect.y + cio(0.5), 3, 3) ..
-        fspec.list(my_inv_name, "result_items", rect.x + cio(6), rect.y + cio(1), 2, 2)
+      return ""
+        .. fspec.list(my_inv_name, "crafting_grid", rect.x + cio(1.5), rect.y, 3, 3)
+        .. fspec.list(my_inv_name, "result_items", rect.x + cio(4.5), rect.y + cio(1), 1, 1)
+        .. fspec.list(my_inv_name, "main", rect.x, rect.y + cio(3), 8, 1)
     elseif loc == "footer" then
-      return fspec.list_ring(my_inv_name, "main") ..
-        fspec.list_ring("current_player", "main") ..
-        fspec.list_ring(my_inv_name, "crafting_grid") ..
-        fspec.list_ring("current_player", "main") ..
-        fspec.list_ring(my_inv_name, "result_items") ..
-        fspec.list_ring("current_player", "main")
+      return fspec.list_ring(my_inv_name, "main")
+        .. fspec.list_ring("current_player", "main")
+        .. fspec.list_ring(my_inv_name, "crafting_grid")
+        .. fspec.list_ring("current_player", "main")
+        .. fspec.list_ring(my_inv_name, "result_items")
+        .. fspec.list_ring("current_player", "main")
     end
     return ""
   end)
@@ -57,11 +57,11 @@ local function on_construct(pos)
 
   inv:set_size("main", 8)
   inv:set_size("crafting_grid", 9)
-  inv:set_size("result_items", 4)
+  inv:set_size("result_items", 1)
 end
 
 local function on_rightclick(pos, node, user)
-  local formspec_name = "yatm_woodcraft:crafting_table_formspec:" .. minetest.pos_to_string(pos)
+  local formspec_name = mod:make_name("crafting_table:" .. minetest.pos_to_string(pos))
 
   minetest.show_formspec(
     user:get_player_name(),
@@ -70,12 +70,85 @@ local function on_rightclick(pos, node, user)
   )
 end
 
+local function get_craft_result(pos)
+  local meta = minetest.get_meta(pos)
+  local inv = meta:get_inventory()
+
+  local items = inv:get_list("crafting_grid")
+
+  return minetest.get_craft_result({
+    method = "normal",
+    width = 3,
+    items = items,
+  })
+end
+
+local function refresh_craft_result(pos)
+  local meta = minetest.get_meta(pos)
+  local inv = meta:get_inventory()
+  local output = get_craft_result(pos)
+
+  inv:set_stack("result_items", 1, output.item)
+end
+
+local function consume_craft_recipe(pos)
+  local meta = minetest.get_meta(pos)
+  local inv = meta:get_inventory()
+  local _output, decremented_input = get_craft_result(pos)
+
+  inv:set_list("crafting_grid", decremented_input.items)
+end
+
+local function on_metadata_inventory_move(
+  pos,
+  from_list,
+  from_index,
+  to_list,
+  to_index,
+  count,
+  player
+)
+  if from_list == "result_items" and to_list == "crafting_grid" then
+    consume_craft_recipe(pos)
+  end
+
+  if from_list == "crafting_grid" or to_list == "crafting_grid" then
+    refresh_craft_result(pos)
+  end
+end
+
+local function on_metadata_inventory_put(
+  pos,
+  listname,
+  index,
+  item_stack,
+  player
+)
+  if listname == "crafting_grid" then
+    refresh_craft_result(pos)
+  end
+end
+
+local function on_metadata_inventory_take(
+  pos,
+  listname,
+  index,
+  item_stack,
+  player
+)
+  if listname == "crafting_grid" then
+    refresh_craft_result(pos)
+  elseif listname == "result_items" then
+    consume_craft_recipe(pos)
+  end
+end
+
 mod:register_node("crafting_table_wood", {
-  basename = "yatm_woodcraft:crafting_table",
+  basename = mod:make_name("crafting_table"),
 
   description = mod.S("Wood Crafting Table"),
 
-  codex_entry_id = "yatm_woodcraft:crafting_table",
+  codex_entry_id = mod:make_name("crafting_table"),
 
   groups = {
     choppy = nokore.dig_class("wme"),
@@ -90,7 +163,7 @@ mod:register_node("crafting_table_wood", {
     "yatm_crafting_table_side.png^[transformFX",
     "yatm_crafting_table_side.png"
   },
-  paramtype = "light",
+  paramtype = "none",
   paramtype2 = "facedir",
   drawtype = "nodebox",
   node_box = table_nodebox,
@@ -98,13 +171,9 @@ mod:register_node("crafting_table_wood", {
   sounds = yatm.node_sounds:build("wood"),
 
   on_construct = on_construct,
-  --on_destruct = crafting_table_on_destruct,
-
   on_rightclick = on_rightclick,
-  --allow_metadata_inventory_move = crafting_table_allow_metadata_inventory_move,
-  --allow_metadata_inventory_put = crafting_table_allow_metadata_inventory_put,
-  --allow_metadata_inventory_take = crafting_table_allow_metadata_inventory_take,
 
-  --on_metadata_inventory_put = crafting_table_on_metadata_inventory_put,
-  --on_metadata_inventory_take = crafting_table_on_metadata_inventory_take,
+  on_metadata_inventory_move = on_metadata_inventory_move,
+  on_metadata_inventory_put = on_metadata_inventory_put,
+  on_metadata_inventory_take = on_metadata_inventory_take,
 })
