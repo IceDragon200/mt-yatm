05faf97c5c0a6a266e07f8144f01bdcee2640986
Corey Powell <mistdragon100@gmail.com>
2024-02-16 19:53:47 -0500

    yatm_fluids: Some function specs and minor optimizations

--- yatm_fluids/fluid_tanks.lua
+++ yatm_fluids/fluid_tanks.lua
@@ -3,6 +3,7 @@
 --
 local FluidStack = assert(yatm_fluids.FluidStack)
 
+--- @namespace yatm_fluids.FluidTanks
 local FluidTanks = {
   version = "1.2.0"
 }
@@ -11,13 +12,16 @@ function FluidTanks.has_fluid_interface(pos, dir)
   local node = minetest.get_node(pos)
   local nodedef = minetest.registered_nodes[node.name]
 
-  if nodedef and nodedef.fluid_interface then
-    return true
+  if nodedef then
+    if nodedef.fluid_interface then
+      return true
+    end
+    return nil, "no fluid_interface"
   end
-  return nil, "no nodedef, or fluid_interface"
+  return nil, "no nodedef"
 end
 
---@since "1.2.0"
+--- @since "1.2.0"
 function FluidTanks.get_fluid_interface(pos)
   local node = minetest.get_node(pos)
   local nodedef = minetest.registered_nodes[node.name]
@@ -28,26 +32,37 @@ function FluidTanks.get_fluid_interface(pos)
   return nil, "no fluid interface"
 end
 
+--- @spec get_capacity(pos: Vector3, dir: Direction): (Integer, nil) | (nil, String)
 function FluidTanks.get_capacity(pos, dir)
   if type(dir) ~= "number" then
     error("expected a number got:" .. type(dir))
   end
   local node = minetest.get_node(pos)
   local nodedef = minetest.registered_nodes[node.name]
-  if nodedef and nodedef.fluid_interface and nodedef.fluid_interface.get_capacity then
-    return nodedef.fluid_interface:get_capacity(pos, dir)
+  if nodedef then
+    local fi = nodedef.fluid_interface
+    if fi and fi.get_capacity then
+      return nodedef.fluid_interface:get_capacity(pos, dir)
+    else
+      return "no fluid_interface:get_capacity function"
+    end
+    return "no fluid_interface"
   end
-  return nil, "no nodedef, fluid_interface or get_capacity function"
+  return nil, "no nodedef"
 end
 
+--- @spec get_fluid(pos: Vector3, dir: Direction): (FluidStack, nil) | (nil, String)
 function FluidTanks.get_fluid(pos, dir)
   if type(dir) ~= "number" then
     error("expected a number got:" .. type(dir))
   end
   local node = minetest.get_node(pos)
   local nodedef = minetest.registered_nodes[node.name]
-  if nodedef and nodedef.fluid_interface and nodedef.fluid_interface.get then
-    return nodedef.fluid_interface:get(pos, dir)
+  if nodedef then
+    local fi = nodedef.fluid_interface
+    if fi and fi.get then
+      return nodedef.fluid_interface:get(pos, dir)
+    end
   end
   return nil, "fluid_interface unavailable"
 end
@@ -61,8 +76,9 @@ function FluidTanks.replace_fluid(pos, dir, fluid_stack, commit)
   local nodedef = minetest.registered_nodes[node.name]
 
   if nodedef and nodedef.groups.fluid_interface_in then
-    if nodedef.fluid_interface and nodedef.fluid_interface.replace then
-      return nodedef.fluid_interface:replace(pos, dir, fluid_stack, commit)
+    local fi = nodedef.fluid_interface
+    if fi and fi.replace then
+      return fi:replace(pos, dir, fluid_stack, commit)
     end
   end
 
@@ -82,18 +98,31 @@ function FluidTanks.drain_fluid(pos, dir, fluid_stack, commit)
   local nodedef = minetest.registered_nodes[node.name]
 
   if nodedef and nodedef.groups.fluid_interface_out then
-    if nodedef.fluid_interface and nodedef.fluid_interface.drain then
-      return nodedef.fluid_interface:drain(pos, dir, fluid_stack, commit)
+    local fi = nodedef.fluid_interface
+    if fi and fi.drain then
+      return fi:drain(pos, dir, fluid_stack, commit)
     end
   end
 
   return nil, "fluid_interface unavailable"
 end
 
+--- Attempts to fill the FluidTank at specified location.
+--- This function returns the amount of fluid actually used.
+--- If the tank rejected the fluid or any other error occured nil will be returned instead.
+---
+--- @spec fill_fluid(
+---   pos: Vector3,
+---   dir: foundation.com.Direction,
+---   fluid_stack: FluidStack,
+---   commit: Boolean
+--- ): (FluidStack, nil) | (nil, error: String)
 function FluidTanks.fill_fluid(pos, dir, fluid_stack, commit)
   if type(dir) ~= "number" then
     error("expected a number got:" .. type(dir))
   end
+
+  assert(fluid_stack, "expected a FluidStack")
 
   if fluid_stack.amount <= 0 then
     return nil, "fluid stack was empty"
@@ -110,15 +139,16 @@ function FluidTanks.fill_fluid(pos, dir, fluid_stack, commit)
     return nil, "fluid_interface does not support input"
   end
 
-  if not nodedef.fluid_interface then
+  local fi = nodedef.fluid_interface
+  if not fi then
     return nil, "fluid_interface unavailable"
   end
 
-  if not nodedef.fluid_interface.fill then
+  if not fi.fill then
     return nil, "fluid_interface does not have fill function"
   end
 
-  return nodedef.fluid_interface:fill(pos, dir, fluid_stack, commit)
+  return fi:fill(pos, dir, fluid_stack, commit)
 end
 
 function FluidTanks.trigger_on_fluid_changed(pos, dir, fluid_stack)
