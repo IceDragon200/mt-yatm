edbfd2e54da69e42c17b0b88248643fa85087653
Corey Powell <mistdragon100@gmail.com>
2025-08-17 22:13:33 -0500

    yatm_brewery_apple_cider: Added apple_juice fluid to serve as base for aging recipes

--- yatm_brewery_apple_cider/fluids.lua
+++ yatm_brewery_apple_cider/fluids.lua
@@ -1,10 +1,34 @@
-yatm.fluids.fluid_registry.register("yatm_brewery_apple_cider", "apple_cider", {
-  description = "Apple Cider",
+local mod = assert(yatm_brewery_apple_cider)
 
-  color = "#FFFFFF",
+--- Sweet Cider effectively
+yatm.fluids.fluid_registry.register("yatm", "apple_juice", {
+  description = mod.S("Apple Juice"),
+
+  color = "#D2B48C",
 
   groups = {
-    apple = 1,
+    flavor_apple = 1,
+    juice = 1,
+  },
+
+  tiles = {
+    source = "yatm_apple_cider_source.png",
+    flowing = "yatm_apple_cider_flowing.png",
+  },
+
+  fluid_tank = {
+    groups = { juice_tank = 1 },
+  },
+})
+
+--- Hard Cider
+yatm.fluids.fluid_registry.register("yatm", "apple_cider", {
+  description = mod.S("Apple Cider"),
+
+  color = "#966F33",
+
+  groups = {
+    flavor_apple = 1,
     alcoholic = 1,
     cider = 1,
     booze = 1,
