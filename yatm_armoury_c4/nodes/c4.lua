933b6e0477cf4974d8845c6a70a6c6652892b97c
Corey Powell <mistdragon100@gmail.com>
2025-08-22 01:13:43 -0500

    yatm_armoury_c4: Remote C4s should use the radio network

--- yatm_armoury_c4/nodes/c4.lua
+++ yatm_armoury_c4/nodes/c4.lua
@@ -34,38 +34,47 @@ mod:register_node("c4_tripwired", {
   },
 })
 
-mod:register_node("c4_remote", {
-  description = mod.S("C4 (Remote Operated)"),
+if foundation.is_module_present("yatm_radio_network") then
+  mod:register_node("c4_remote", {
+    description = mod.S("C4 (Remote Operated)"),
 
-  groups = {
-    cracky = nokore.dig_class("wme"),
-    oddly_breakable_by_hand = nokore.dig_class("hand"),
-    --
-    c4 = 1,
-    explosive = 1,
-    remote_operated = 1,
-  },
+    groups = {
+      cracky = nokore.dig_class("wme"),
+      oddly_breakable_by_hand = nokore.dig_class("hand"),
+      --
+      c4 = 1,
+      explosive = 1,
+      remote_operated = 1,
+    },
 
-  paramtype = "light",
-  paramtype2 = "facedir",
+    paramtype = "light",
+    paramtype2 = "facedir",
 
-  drawtype = "nodebox",
-  node_box = {
-    type = "fixed",
-    fixed = {
-      {-8/16,-8/16,-6/16,8/16,-3/16,6/16}, -- block
-      {-6/16,-6/16,-7/16,0/16,-1/16,7/16}, -- remote receiver
-      {-7/16,-3/16, 4/16,5/16,-2/16, 5/16}, -- antena
-    }
-  },
+    drawtype = "nodebox",
+    node_box = {
+      type = "fixed",
+      fixed = {
+        {-8/16,-8/16,-6/16,8/16,-3/16,6/16}, -- block
+        {-6/16,-6/16,-7/16,0/16,-1/16,7/16}, -- remote receiver
+        {-7/16,-3/16, 4/16,5/16,-2/16, 5/16}, -- antena
+      }
+    },
 
-  use_texture_alpha = "opaque",
-  tiles = {
-    "yatm_c4_remote.top.png",
-    "yatm_c4_remote.bottom.png",
-    "yatm_c4_remote.side.png",
-    "yatm_c4_remote.side.png^[transformFX",
-    "yatm_c4_remote.front.png^[transformFX",
-    "yatm_c4_remote.front.png",
-  },
-})
+    use_texture_alpha = "opaque",
+    tiles = {
+      "yatm_c4_remote.top.png",
+      "yatm_c4_remote.bottom.png",
+      "yatm_c4_remote.side.png",
+      "yatm_c4_remote.side.png^[transformFX",
+      "yatm_c4_remote.front.png^[transformFX",
+      "yatm_c4_remote.front.png",
+    },
+
+    radio_network = {
+      on_message = function (self, pos, node, message)
+        if message == "DETONATE" then
+        end
+      end,
+    },
+  })
+end
