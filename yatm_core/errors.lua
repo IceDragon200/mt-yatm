e6087e3c309d603fa636836723a01d26063869fa
Corey Powell <mistdragon100@gmail.com>
2025-09-02 00:02:36 -0500

    yatm_core: Avoid reporting yatm's utility log functions as the caller

--- yatm_core/errors.lua
+++ yatm_core/errors.lua
@@ -1,16 +1,16 @@
 function yatm.info(message)
-  core.log("info", message)
+  core.log("info", message, 2)
 end
 
 function yatm.warn(message)
-  core.log("warning", message)
+  core.log("warning", message, 2)
 end
 
 function yatm.error(message)
   if yatm.config.fail_loud then
     error("ERROR: " .. message)
   else
-    core.log("error", message)
+    core.log("error", message, 2)
   end
 end
 
