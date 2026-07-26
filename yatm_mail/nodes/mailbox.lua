96945d9dc82a4df671f27a67397e52f8a5769863
Corey Powell <mistdragon100@gmail.com>
2026-07-26 13:24:14 -0500

    yatm_mail: Just inline the label

--- yatm_mail/nodes/mailbox.lua
+++ yatm_mail/nodes/mailbox.lua
@@ -70,8 +70,7 @@ local function mailbox_get_formspec(user, assigns)
 
   return yatm.formspec_render_split_inv_panel(user, nil, 4, { bg = bg }, function (slot, rect)
     if slot == "main_body" then
-      local formspec =
-        fspec.label(0, 0, "Mailbox")
+      local formspec = fspec.label(rect.x, rect.y, "Mailbox")
 
       if yatm_security.is_lockable_node(pos) then
         -- if it's lockable, show the access key slot
